eabf71e0d09575feb90326acbf62ecde663227cb
delucis <swithinbank@gmail.com>
2022-03-17 00:15:34 +0100

    Create utility for getting translated string
    This is to supplement the `UIString` Astro component as that can’t be used inside framework component props.

--- src/i18n/util.ts
+++ src/i18n/util.ts
@@ -1,3 +1,5 @@
+import type { AstroGlobal } from 'astro';
+import { getLanguageFromURL } from '../util';
 import { translations } from './translations';
 
 const fallbackLang = 'en';
@@ -6,4 +8,21 @@ type Keys = keyof typeof translations[typeof fallbackLang];
 
 export function getLanguageString(key: Keys, lang = 'en'): string | undefined {
 	return translations[lang]?.[key] || translations[fallbackLang][key];
+}
+
+/**
+ * Get a UI string to pass to a framework component.
+ * 
+ * Within an Astro component, prefer the `UIString` component,
+ * which only needs the key as it has access to the global Astro object.
+ * 
+ * However, you can’t pass an Astro component as a prop to a framework component,
+ * so this function is provided to get the string instead:
+ * 
+ * @example
+ * <FrameworkComponent label={UIString('new', Astro)} />
+ */
+export function UIString(key: Keys, Astro: Readonly<AstroGlobal>) {
+	const lang = getLanguageFromURL(Astro.request.canonicalURL.pathname);
+	return getLanguageString(key, lang);
 }
