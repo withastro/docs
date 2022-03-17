8ce7a8847f0116cb2632992a731265053079df1f
Fred K. Schott <fkschott@gmail.com>
2022-03-17 11:45:27 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const SIDEBAR = {
 		{ text: 'Astro vs. X', link: 'en/comparing-astro-vs-other-tools' },
 
 		{ text: 'Core Concepts', header: true, type: 'learn' },
-		{ text: 'Why Astro?', link: 'en/core-concepts/why-astro' },
+		// ADD: Why Astro?
 		{ text: 'Partial Hydration', link: 'en/core-concepts/partial-hydration' },
 		// ADD: Island Architecture
 
