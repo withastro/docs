6346eeeea17677aeea23a67f68bb278caee964c1
Sarah Rainsberger <sarah@rainsberger.ca>
2022-03-23 12:11:00 +0000

    fix toggle links

--- src/components/TabGroup/InstallGuideTabGroup.tsx
+++ src/components/TabGroup/InstallGuideTabGroup.tsx
@@ -4,10 +4,10 @@ import './TabGroup.css';
 const InstallGuideTabGroup = ({ currentPage }) => {
 	return (
 		<div class="InstallGuideTabGroup TabGroup">
-			<a href="auto" class={currentPage.endsWith('auto') ? 'active' : ''} onClick={() => toggleType('auto')}>
+			<a href="/en/install/auto" class={currentPage.endsWith('auto') ? 'active' : ''} onClick={() => toggleType('auto')}>
 				Automatic CLI
 			</a>
-			<a href="manual" class={currentPage.endsWith('manual') ? 'active' : ''} onClick={() => toggleType('manual')}>
+			<a href="/en/install/manual" class={currentPage.endsWith('manual') ? 'active' : ''} onClick={() => toggleType('manual')}>
 				Manual Setup
 			</a>
 		</div>
