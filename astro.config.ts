5e34c526f94190eb57a772b3bc595d6c16ed71d6
Fred K. Schott <fkschott@gmail.com>
2022-03-26 22:12:49 -0700

    update astro@0.25

--- astro.config.ts
+++ astro.config.ts
@@ -1,32 +1,25 @@
 import { tokens } from './syntax-highlighting-theme';
-import type { AstroUserConfig } from 'astro';
 import type { AstroMarkdownOptions } from '@astrojs/markdown-remark';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
 
-const config: AstroUserConfig = {
-	buildOptions: {
-		site: 'https://docs.astro.build/',
-	},
-	markdownOptions: {
-		render: [
-			'@astrojs/markdown-remark',
-			{
-				syntaxHighlight: 'shiki',
-				shikiConfig: {
-					theme: {
-						name: 'Star gazer',
-						type: 'dark',
-						settings: tokens,
-					},
-				},
-			} as AstroMarkdownOptions,
-		],
-	},
-	renderers: [
-		// Our main renderer for frontend components
-		'@astrojs/renderer-preact',
-		// Needed for Algolia search component
-		'@astrojs/renderer-react',
-	],
-};
-
-export default config;
+// https://astro.build/config
+export default defineConfig({
+  buildOptions: {
+    site: 'https://docs.astro.build/'
+  },
+  markdownOptions: {
+    render: ['@astrojs/markdown-remark', ({
+      syntaxHighlight: 'shiki',
+      shikiConfig: {
+        theme: {
+          name: 'Star gazer',
+          type: 'dark',
+          settings: tokens
+        }
+      }
+    } as AstroMarkdownOptions)]
+  },
+  integrations: [preact(), react()]
+});
