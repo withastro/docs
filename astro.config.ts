f4cd56c2bda623cc9c2f99474c0c634300ee0e5f
JuanM04 <me@juanm04.com>
2022-04-02 20:30:21 -0300

    Updaded astro config

--- astro.config.ts
+++ astro.config.ts
@@ -1,46 +1,42 @@
-import { tokens } from './syntax-highlighting-theme';
-import type { AstroMarkdownOptions } from '@astrojs/markdown-remark';
+import { tokens, foregroundPrimary, backgroundPrimary } from './syntax-highlighting-theme';
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	buildOptions: {
-		site: 'https://docs.astro.build/',
-	},
-	markdownOptions: {
-		render: [
-			'@astrojs/markdown-remark',
-			{
-				syntaxHighlight: 'shiki',
-				shikiConfig: {
-					theme: {
-						name: 'Star gazer',
-						type: 'dark',
-						settings: tokens,
+	site: 'https://docs.astro.build/',
+	integrations: [preact(), react()],
+	markdown: {
+		// @ts-expect-error This will be fixed by either #2970 or #2971
+		mode: 'mdx',
+		syntaxHighlight: 'shiki',
+		shikiConfig: {
+			theme: {
+				name: 'Star gazer',
+				type: 'dark',
+				settings: tokens,
+				fg: foregroundPrimary,
+				bg: backgroundPrimary,
+			},
+		},
+		rehypePlugins: [
+			// These are here because setting custom plugins disables the defualt plugins
+			'rehype-slug',
+			'remark-smartypants',
+			'remark-gfm',
+			// This adds links to headings
+			[
+				'rehype-autolink-headings',
+				{
+					properties: {
+						class: 'heading-fragment-link',
+						title: 'Permalink to this heading',
 					},
+					behavior: 'append',
+					content: { type: 'text', value: '¶' },
 				},
-				rehypePlugins: [
-					// These are here because setting custom plugins disables the defualt plugins
-					'rehype-slug',
-					'remark-smartypants',
-					'remark-gfm',
-					// This adds links to headings
-					[
-						'rehype-autolink-headings',
-						{
-							properties: {
-								class: 'heading-fragment-link',
-								title: 'Permalink to this heading',
-							},
-							behavior: 'append',
-							content: { type: 'text', value: '¶' },
-						},
-					],
-				],
-			} as AstroMarkdownOptions,
+			],
 		],
 	},
-	integrations: [preact(), react()],
 });
