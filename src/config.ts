4435e484cea04f892eb3793f387a4c164c135ea7
Astroalex <alexis31.balthazard@gmail.com>
2022-04-09 01:15:45 +0200

    commenting every languages pages other than `getting-started` for now

--- src/config.ts
+++ src/config.ts
@@ -52,96 +52,102 @@ export const SIDEBAR = {
 	de: [
 		{ text: 'Einrichtung', header: true, type: 'learn' },
 		{ text: 'Erste Schritte', link: 'de/getting-started' },
-		{ text: 'Schnellstart', link: 'de/quick-start' },
-		{ text: 'Installation', link: 'de/installation' },
-		{ text: 'Astro vs. X', link: 'de/comparing-astro-vs-other-tools' },
-		{ text: 'Umstellung auf v0.21', link: 'de/migration/0.21.0' },
+		// Comment this out when the translation is up-to-date ! :D
+		// { text: 'Schnellstart', link: 'de/quick-start' },
+		// { text: 'Installation', link: 'de/installation' },
+		// { text: 'Astro vs. X', link: 'de/comparing-astro-vs-other-tools' },
+		// { text: 'Umstellung auf v0.21', link: 'de/migration/0.21.0' },
 
-		{ text: 'Grundlagen', header: true, type: 'learn' },
-		{ text: 'Projektstruktur', link: 'de/core-concepts/project-structure' },
-		{ text: 'Astro-Komponenten', link: 'de/core-concepts/astro-components' },
-		{ text: 'Astro-Seiten', link: 'de/core-concepts/astro-pages' },
-		{ text: 'Layouts', link: 'de/core-concepts/layouts' },
-		{ text: 'Routing', link: 'de/core-concepts/routing' },
-		{ text: 'Partial Hydration', link: 'de/core-concepts/component-hydration' },
+		// { text: 'Grundlagen', header: true, type: 'learn' },
+		// { text: 'Projektstruktur', link: 'de/core-concepts/project-structure' },
+		// { text: 'Astro-Komponenten', link: 'de/core-concepts/astro-components' },
+		// { text: 'Astro-Seiten', link: 'de/core-concepts/astro-pages' },
+		// { text: 'Layouts', link: 'de/core-concepts/layouts' },
+		// { text: 'Routing', link: 'de/core-concepts/routing' },
+		// { text: 'Partial Hydration', link: 'de/core-concepts/component-hydration' },
 
-		{ text: 'Anleitungen', header: true, type: 'learn' },
-		{ text: 'Styling & CSS', link: 'de/guides/styling' },
+		// { text: 'Anleitungen', header: true, type: 'learn' },
+		// { text: 'Styling & CSS', link: 'de/guides/styling' },
 
-		{ text: 'Referenz', header: true, type: 'api' },
+		// { text: 'Referenz', header: true, type: 'api' },
 	],
 	nl: [
 		{ text: 'Welkom', header: true, type: 'learn' },
 		{ text: 'Beginnen', link: 'nl/getting-started' },
-		{ text: 'Snel start', link: 'nl/quick-start' },
+		// Comment this out when the translation is up-to-date ! :D
+		// { text: 'Snel start', link: 'nl/quick-start' },
 	],
 	fi: [
 		{ text: 'Tervetuloa', header: true, type: 'learn' },
 		{ text: 'Aloittaminen', link: 'fi/getting-started' },
-		{ text: 'Pika-aloitus', link: 'fi/quick-start' },
-		{ text: 'Asennus', link: 'fi/installation' },
+		// Comment this out when the translation is up-to-date ! :D
+		// { text: 'Pika-aloitus', link: 'fi/quick-start' },
+		// { text: 'Asennus', link: 'fi/installation' },
 	],
 	es: [
 		{ text: 'Configuración', header: true, type: 'learn' },
 		{ text: 'Empezando', link: 'es/getting-started' },
-		{ text: 'Comienzo rápido', link: 'es/quick-start' },
-		{ text: 'Instalación', link: 'es/installation' },
-		{ text: 'Astro vs. X', link: 'es/comparing-astro-vs-other-tools' },
+		// Comment this out when the translation is up-to-date ! :D
+		// { text: 'Comienzo rápido', link: 'es/quick-start' },
+		// { text: 'Instalación', link: 'es/installation' },
+		// { text: 'Astro vs. X', link: 'es/comparing-astro-vs-other-tools' },
 
-		{ text: 'Fundamentos', header: true, type: 'learn' },
-		{
-			text: 'Estructura del Proyecto',
-			link: 'es/core-concepts/project-structure',
-		},
-		{
-			text: 'Sintaxis del Componente',
-			link: 'es/core-concepts/astro-components',
-		},
-		{ text: 'Páginas', link: 'es/core-concepts/astro-pages' },
-		{ text: 'Maquetas', link: 'es/core-concepts/layouts' },
-		{ text: 'Enrutamiento', link: 'es/core-concepts/routing' },
-		{
-			text: 'Hidratación parcial',
-			link: 'es/core-concepts/component-hydration',
-		},
+		// { text: 'Fundamentos', header: true, type: 'learn' },
+		// {
+		// 	text: 'Estructura del Proyecto',
+		// 	link: 'es/core-concepts/project-structure',
+		// },
+		// {
+		// 	text: 'Sintaxis del Componente',
+		// 	link: 'es/core-concepts/astro-components',
+		// },
+		// { text: 'Páginas', link: 'es/core-concepts/astro-pages' },
+		// { text: 'Maquetas', link: 'es/core-concepts/layouts' },
+		// { text: 'Enrutamiento', link: 'es/core-concepts/routing' },
+		// {
+		// 	text: 'Hidratación parcial',
+		// 	link: 'es/core-concepts/component-hydration',
+		// },
 
-		{ text: 'Guías', header: true, type: 'learn' },
-		{ text: 'Estilo y CSS', link: 'es/guides/styling' },
-		{ text: 'Markdown', link: 'es/guides/markdown-content' },
-		{ text: 'Depuración', link: 'es/guides/debugging' },
-		{ text: 'Obtención de datos', link: 'es/guides/data-fetching' },
-		{ text: 'RSS', link: 'es/guides/rss' },
-		{ text: 'Importaciones admitidas', link: 'es/guides/imports' },
-		{ text: 'Alias', link: 'es/guides/aliases' },
-		{ text: 'Desplegar en la web', link: 'es/guides/deploy' },
-		{ text: 'Publicar en npm', link: 'es/guides/publish-to-npm' },
+		// { text: 'Guías', header: true, type: 'learn' },
+		// { text: 'Estilo y CSS', link: 'es/guides/styling' },
+		// { text: 'Markdown', link: 'es/guides/markdown-content' },
+		// { text: 'Depuración', link: 'es/guides/debugging' },
+		// { text: 'Obtención de datos', link: 'es/guides/data-fetching' },
+		// { text: 'RSS', link: 'es/guides/rss' },
+		// { text: 'Importaciones admitidas', link: 'es/guides/imports' },
+		// { text: 'Alias', link: 'es/guides/aliases' },
+		// { text: 'Desplegar en la web', link: 'es/guides/deploy' },
+		// { text: 'Publicar en npm', link: 'es/guides/publish-to-npm' },
 
-		{ text: 'Referencia', header: true, type: 'api' },
+		// { text: 'Referencia', header: true, type: 'api' },
 
-		{ text: 'Referencia de API', link: 'es/reference/api-reference' },
-		{ text: 'Referencia de CLI', link: 'es/reference/cli-reference' },
-		{
-			text: 'Referencia de configuración',
-			link: 'es/reference/configuration-reference',
-		},
+		// { text: 'Referencia de API', link: 'es/reference/api-reference' },
+		// { text: 'Referencia de CLI', link: 'es/reference/cli-reference' },
+		// {
+		// 	text: 'Referencia de configuración',
+		// 	link: 'es/reference/configuration-reference',
+		// },
 
 	],
 	'zh-CN': [
 		{ text: '起步', header: true, type: 'learn' },
 		{ text: '入门指南', link: 'zh-CN/getting-started' },
-		{ text: '快速入门', link: 'zh-CN/quick-start' },
-		{ text: '安装指南', link: 'zh-CN/installation' },
-		{ text: '模板样例', link: 'zh-CN/examples' },
-		{
-			text: 'Astro 对比其他框架',
-			link: 'zh-CN/comparing-astro-vs-other-tools',
-		},
+		// Comment this out when the translation is up-to-date ! :D
+		// { text: '快速入门', link: 'zh-CN/quick-start' },
+		// { text: '安装指南', link: 'zh-CN/installation' },
+		// { text: '模板样例', link: 'zh-CN/examples' },
+		// {
+		// 	text: 'Astro 对比其他框架',
+		// 	link: 'zh-CN/comparing-astro-vs-other-tools',
+		// },
 	],
 	'zh-TW': [
 		{ text: '設定', header: true, type: 'learn' },
 		{ text: '新手上路', link: 'zh-TW/getting-started' },
-		{ text: '快速開始', link: 'zh-TW/quick-start' },
-		{ text: '安裝', link: 'zh-TW/installation' },
+		// Comment this out when the translation is up-to-date ! :D
+		// { text: '快速開始', link: 'zh-TW/quick-start' },
+		// { text: '安裝', link: 'zh-TW/installation' },
 	],
 	bg: [
 		{ text: 'Главни', header: true, type: 'learn' },
@@ -150,7 +156,7 @@ export const SIDEBAR = {
 	fr: [
 		{ text: 'Bienvenue', header: true, type: 'learn' },
 		{ text: 'Bien démarrer', link: 'fr/getting-started' },
-		// Comment this out we have an actual french translation up-to-date ! :D
+		// Comment this out when the translation is up-to-date ! :D
 		// { text: 'Démarrage rapide', link: 'fr/quick-start' },
 		// { text: 'Installation', link: 'fr/installation' },
 	],
@@ -173,17 +179,19 @@ export const SIDEBAR = {
 	ja: [
 		{ text: 'セットアップ', header: true, type: 'learn' },
 		{ text: 'はじめに', link: 'ja/getting-started' },
-		{ text: 'クイックスタート', link: 'ja/quick-start' },
-		{ text: 'インストール', link: 'ja/installation' },
-		{ text: 'Astro vs. X', link: 'ja/comparing-astro-vs-other-tools' },
+		// Comment this out when the translation is up-to-date ! :D
+		// { text: 'クイックスタート', link: 'ja/quick-start' },
+		// { text: 'インストール', link: 'ja/installation' },
+		// { text: 'Astro vs. X', link: 'ja/comparing-astro-vs-other-tools' },
 
-		{ text: '基本', header: true, type: 'learn' },
-		{ text: 'ディレクトリ構成', link: 'ja/core-concepts/project-structure' },
+		// { text: '基本', header: true, type: 'learn' },
+		// { text: 'ディレクトリ構成', link: 'ja/core-concepts/project-structure' },
 	],
 	ru: [
 		{ text: 'Введение', header: true, type: 'learn' },
 		{ text: 'Начало работы', link: 'ru/getting-started' },
-		{ text: 'Быстрый старт', link: 'ru/quick-start' },
+		// Comment this out when the translation is up-to-date ! :D
+		// { text: 'Быстрый старт', link: 'ru/quick-start' },
 	],
 	it: [
 		{ text: 'Impostare', header: true, type: 'learn' },
@@ -196,8 +204,9 @@ export const SIDEBAR = {
 	hu: [
 		{ text: 'Beállítás', header: true, type: 'learn' },
 		{ text: 'Első Lépések', link: 'hu/getting-started' },
-		{ text: 'Gyors Beállítás', link: 'hu/quick-start' },
-		{ text: 'Telepítés', link: 'hu/installation' },
+		// Comment this out when the translation is up-to-date ! :D
+		// { text: 'Gyors Beállítás', link: 'hu/quick-start' },
+		// { text: 'Telepítés', link: 'hu/installation' },
 	],
 };
 
