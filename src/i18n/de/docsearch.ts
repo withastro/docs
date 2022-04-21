5112864920b2271e13d1ec0834df4add7155ca6b
delucis <swithinbank@gmail.com>
2022-04-22 01:39:47 +0200

    Add German search translation as example

---
+++ src/i18n/de/docsearch.ts
@@ -0,0 +1,8 @@
+import { DocSearchDictionary } from '../checks';
+
+export default DocSearchDictionary({
+	button: 'Suchen',
+	placeholder: 'Dokumentation durchsuchen',
+	shortcutLabel: 'Drücke / zum Suchen',
+	translations: {},
+});
