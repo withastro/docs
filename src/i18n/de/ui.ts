d6f1510e1125bc4cc034be8b1f02138df0815345
delucis <swithinbank@gmail.com>
2022-04-22 17:51:49 +0200

    Restore German.& Portuguese UI translations after merge

--- src/i18n/de/ui.ts
+++ src/i18n/de/ui.ts
@@ -1,6 +1,19 @@
 import { UIDictionary } from '../checks';
 
 export default UIDictionary({
+	'leftSidebar.learnTab': 'Lernen',
+	'leftSidebar.referenceTab': 'Referenz',
+	'rightSidebar.onThisPage': 'Auf dieser Seite',
+	'rightSidebar.overview': 'Überblick',
+	'rightSidebar.more': 'Mehr',
+	'rightSidebar.editPage': 'Bearbeite diese Seite',
+	'rightSidebar.translatePage': 'Übersetze diese Seite',
+	'rightSidebar.joinCommunity': 'Trete unserer Community bei',
+	'articleNav.nextPage': 'Nächste Seite',
+	'articleNav.prevPage': 'Zurück',
+	'since.addedIn': 'Hinzugefügt in:',
+	'since.new': 'Neu',
+	'contributors.seeAll': 'Alle Mitwirkenden ansehen',
 	'404.title': 'Nicht gefunden',
 	'404.content': 'Diese Seite befindet sich nicht in unserem Sonnensystem.',
 	'404.linkText': 'Bring mich nach Hause.',
