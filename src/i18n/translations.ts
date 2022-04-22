18bd924071a3cd2130d4a432ec51151789ece105
Pukima <pukima@pukima.site>
2022-04-22 13:50:59 +0200

    Added german translation in `translation.ts` (#331)
    I know it's just a small contribution but I hope it helps <3

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -40,7 +40,21 @@ const checkLanguages = <T extends Record<LanguagesInUse, Partial<typeof en>>>(co
 
 export const translations = checkLanguages({
 	en,
-	de: {},
+	de: {
+		'leftSidebar.learnTab': 'Lernen',
+		'leftSidebar.referenceTab': 'Referenz',
+		'rightSidebar.onThisPage': 'Auf dieser Seite',
+		'rightSidebar.overview': 'Überblick',
+		'rightSidebar.more': 'Mehr',
+		'rightSidebar.editPage': 'Bearbeite diese Seite',
+		'rightSidebar.translatePage': 'Übersetze diese Seite',
+		'rightSidebar.joinCommunity': 'Trete unserer Community bei',
+		'articleNav.nextPage': 'Nächste Seite',
+		'articleNav.prevPage': 'Zurück',
+		'since.addedIn': 'Hinzugefügt in:',
+		'since.new': 'Neu',
+		'contributors.seeAll': 'Alle Mitwirkenden ansehen',
+	},
 	nl: {},
 	'pt-BR': {},
 	fi: {},
