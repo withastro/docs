ad600879801c27ab74cf272f208694e8be5a8d26
Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>
2022-05-05 11:22:07 -0300

    i18n(pt-BR): Updated Getting Started and UI/Docsearch. (#444)
    * i18n(pt-BR): Updated Getting Started and UI/Docsearch
    
    Updates the pt-BR getting started page and `docsearch.ts` and `ui.ts` texts.
    
    * Add missing comma
    
    Co-authored-by: Caio Ferrarezi <caioferrarezi2@gmail.com>
    
    Co-authored-by: Caio Ferrarezi <caioferrarezi2@gmail.com>

--- src/i18n/pt-BR/ui.ts
+++ src/i18n/pt-BR/ui.ts
@@ -1,6 +1,15 @@
 import { UIDictionary } from '../translation-checkers';
 
 export default UIDictionary({
+	'a11y.skipLink': 'Pular para o Conteúdo',
+	'navbar.a11yTitle': 'Navegação Superior',
+	'site.title': 'Documentação do Astro',
+	'site.description': 'Construa websites mais rápidos com menos Javascript no lado do cliente.',
+	'site.og.imageSrc': '/default-og-image.png?v=1',
+	'site.og.imageAlt': 'a logo do astro em um estrelado pedaço do espaço, com um planeta roxo parecido com Saturno flutuando à direita',
+	'leftSidebar.a11yTitle': 'Navegação do site',
+	'leftSidebar.noTranslations': 'Não foram encontradas traduções',
+	'leftSidebar.viewInEnglish': 'Veja em Inglês',
 	'leftSidebar.learnTab': 'Aprenda',
 	'leftSidebar.referenceTab': 'Referência',
 	'rightSidebar.onThisPage': 'Nesta página',
@@ -9,8 +18,17 @@ export default UIDictionary({
 	'rightSidebar.editPage': 'Edite esta página',
 	'rightSidebar.translatePage': 'Traduza esta página',
 	'rightSidebar.joinCommunity': 'Junte-se à comunidade',
+	'rightSidebar.a11yTitle': 'Índice',
 	'articleNav.nextPage': 'Próxima página',
 	'articleNav.prevPage': 'Página anterior',
 	'since.addedIn': 'Adicionado em:',
 	'since.new': 'Novo',
+	'install.autoTab': 'Interface de Linha de Comando Automática',
+	'install.manualTab': 'Instalação Manual',
+	'contributors.seeAll': 'Veja todos os contribuidores',
+	'fallbackContent.notice': 'Esta página ainda não está disponível em sua língua, portanto estamos te mostrando a versão em Inglês. Você pode nos ajudar a traduzindo!',
+	'fallbackContent.linkText': 'Aprenda mais sobre como você pode contribuir',
+	'404.title': 'Não encontrado',
+	'404.content': 'Esta página não está no nosso sistema solar.',
+	'404.linkText': 'Me leve para casa.',
 });
