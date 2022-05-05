f591fb72578e3c107a2f8b2b9473d4d104d69721
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-05-05 18:31:26 -0400

    translate auto.md ui

--- src/i18n/es/ui.ts
+++ src/i18n/es/ui.ts
@@ -1,4 +1,7 @@
 import { UIDictionary } from '../translation-checkers';
 
 export default UIDictionary({
+	// Installation Guide
+	'install.autoTab': 'CLI Automatico',
+	'install.manualTab': 'Instalación Manual',
 });
