affbfbd2faf1026c91921ab94321371e25e6eb40
Astroalex <alexis31.balthazard@gmail.com>
2022-05-05 22:15:32 +0200

    i18n(fr): Updating `getting-started.md` and UI elements translation.  (#449)
    * Update `getting-started.md` for i18n-fr translation
    
    * Adding UI component translations
    
    * renaming `translations` to `modal` to reflect changes from withastro#452
    
    * Apply suggestions from code review
    
    Co-authored-by: Florian S. <2287301+florian-s-code@users.noreply.github.com>
    
    Co-authored-by: Florian S. <2287301+florian-s-code@users.noreply.github.com>

--- src/i18n/fr/ui.ts
+++ src/i18n/fr/ui.ts
@@ -1,4 +1,43 @@
 import { UIDictionary } from '../translation-checkers';
 
 export default UIDictionary({
+	'a11y.skipLink': 'Aller au contenu principal',
+	'navbar.a11yTitle': 'Navigation principale',
+	// Site settings
+	'site.title': 'Documentation Astro',
+	'site.description': 'Compilez des sites plus rapidement avec moins de JavaScript pour vos utilisateurs.',
+	'site.og.imageSrc': '/default-og-image.png?v=1',
+	'site.og.imageAlt': "Logo d'Astro dans l'espace, avec une planète violette dans le style de saturne flottant à droite de l'image.",
+	// Left Sidebar
+	'leftSidebar.a11yTitle': 'Navigation du site',
+	'leftSidebar.learnTab': 'Apprendre',
+	'leftSidebar.referenceTab': 'Référence',
+	'leftSidebar.noTranslations': 'Aucune traduction trouvée.',
+	'leftSidebar.viewInEnglish': 'Voir en anglais',
+	// Right Sidebar
+	'rightSidebar.a11yTitle': 'Table des matières',
+	'rightSidebar.onThisPage': 'Sur cette page',
+	'rightSidebar.overview': 'Vue générale',
+	'rightSidebar.more': 'Plus',
+	'rightSidebar.editPage': 'Modifier cette page',
+	'rightSidebar.translatePage': 'Traduire cette page',
+	'rightSidebar.joinCommunity': 'Rejoindre notre communauté',
+	// Used in previous/next page links at the bottom of pages
+	'articleNav.nextPage': 'Page suivante',
+	'articleNav.prevPage': 'Page précédente',
+	// Used in `<Since>`: Added in: v0.24.0 [NEW]
+	'since.addedIn': 'Ajouté à la version :',
+	'since.new': 'Nouveau',
+	// Installation Guide
+	'install.autoTab': "Automatiquement via l'ILC",
+	'install.manualTab': 'Configuration manuelle',
+	// `<ContributorList>` fallback text
+	'contributors.seeAll': 'Voir tous les contributeurs',
+	// Fallback content notice shown when a page is not yet translated
+	'fallbackContent.notice': "Cette page est affichée en anglais car elle n'est pas encore disponible dans votre langue. Vous pouvez aider en la traduisant !",
+	'fallbackContent.linkText': 'En savoir plus sur la façon de contribuer',
+	// 404 Page
+	'404.title': 'Page introuvable',
+	'404.content': 'Cette page ne fait pas partie de notre système solaire.',
+	'404.linkText': 'Ramenez moi à la maison',
 });
