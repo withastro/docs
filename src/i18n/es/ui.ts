ae4f63cfbae96a0d02b201ea94c964aaacec8bc9
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-05-06 14:09:21 -0400

    translate ui.ts

--- src/i18n/es/ui.ts
+++ src/i18n/es/ui.ts
@@ -1,7 +1,44 @@
 import { UIDictionary } from '../translation-checkers';
 
 export default UIDictionary({
+	'a11y.skipLink': 'Saltearse al contenido',
+	'navbar.a11yTitle': 'Navigación superior',
+	// Site settings
+	'site.title': 'Documentación de Astro',
+	'site.description': 'Construye páginas web más rápidas con menos Javascript en el lado del cliente.',
+	'site.og.imageSrc': '/default-og-image.png?v=1',
+	'site.og.imageAlt': 'Logo de Astro en el espacio estrellado, con un planeta purpura parecido a Saturno flotando en el fondo a la derecha.',
+	// Left Sidebar
+	'leftSidebar.a11yTitle': 'Navegación de la página',
+	'leftSidebar.learnTab': 'Aprenda',
+	'leftSidebar.referenceTab': 'Referencias',
+	'leftSidebar.noTranslations': 'No se encontraron traducciones',
+	'leftSidebar.viewInEnglish': 'Ver en inglés',
+	// Right Sidebar
+	'rightSidebar.a11yTitle': 'Índice',
+	'rightSidebar.onThisPage': 'En esta página',
+	'rightSidebar.overview': 'Sinopsis',
+	'rightSidebar.more': 'Más recursos',
+	'rightSidebar.editPage': 'Edita esta página',
+	'rightSidebar.translatePage': 'Traduce esta página',
+	'rightSidebar.joinCommunity': 'Únase a la comunidad',
+	'rightSidebar.github': 'Documentación de Astro en GitHub',
+	// Used in previous/next page links at the bottom of pages
+	'articleNav.nextPage': 'Siguiente página',
+	'articleNav.prevPage': 'Atras',
+	// Used in `<Since>`: Added in: v0.24.0 [NEW]
+	'since.addedIn': 'Agregado en:',
+	'since.new': 'Nuevo',
 	// Installation Guide
 	'install.autoTab': 'CLI Automático',
 	'install.manualTab': 'Instalación Manual',
+	// `<ContributorList>` fallback text
+	'contributors.seeAll': 'Ver a todos los contribuidores',
+	// Fallback content notice shown when a page is not yet translated
+	'fallbackContent.notice': 'Esta página aún no está disponible en su idioma, mientras tanto verá la versión en inglés. ¡Ayúdanos a traducirla!',
+	'fallbackContent.linkText': 'Más información sobre cómo contribuir',
+	// 404 Page
+	'404.title': 'No encontrado',
+	'404.content': 'Esta página no está en nuestro sistema solar.',
+	'404.linkText': 'Volver a la página principal.',
 });
