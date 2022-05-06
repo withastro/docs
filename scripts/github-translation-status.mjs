2e64bc753bf28b97c9e11d950a46c16be5bdf459
hippotastic <6137925+hippotastic@users.noreply.github.com>
2022-05-06 20:06:06 +0200

    fix: allow to run script locally w/o env change

--- scripts/github-translation-status.mjs
+++ scripts/github-translation-status.mjs
@@ -470,7 +470,7 @@ const githubTranslationStatus = new GitHubTranslationStatus({
 	targetLanguages: ['de', 'es', 'fr', 'ja', 'pt-BR', 'zh-CN'],
 	languageLabels: { de: 'Deutsch', es: 'Español', fr: 'Français', ja: '日本語', 'pt-BR': 'Português do Brasil', 'zh-CN': '简体中文' },
 	githubToken: process.env.GITHUB_TOKEN,
-	githubRepo: process.env.GITHUB_REPOSITORY,
+	githubRepo: process.env.GITHUB_REPOSITORY || 'withastro/docs',
 	githubRefName: process.env.GITHUB_REF_NAME,
 	issueTitle: '[i18n] Translation Status Overview',
 });
