962adee200094d91f96d35e3c8eb8483b7bf9766
Yan Thomas <yanthomas.b@gmail.com>
2022-05-08 01:32:20 +0000

    i18n(pt-BR): Adding `editor-setup.md` & `integrations.md` translations

--- src/i18n/pt-BR/nav.ts
+++ src/i18n/pt-BR/nav.ts
@@ -3,5 +3,7 @@ import { NavDictionary } from '../translation-checkers';
 export default NavDictionary({
 	startHere: 'Instalação',
 	'getting-started': 'Introdução',
-	'install': 'Instalação'
+	'install': 'Instalação',
+	'editor-setup': 'Configuração do Editor',
+	'integrations/integrations': 'Feito com Astro'
 });
