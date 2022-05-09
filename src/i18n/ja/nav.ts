a8e5f07059ef011f462f9526adfc00fde5020032
Kyosuke <kyosuke@users.noreply.github.com>
2022-05-09 09:16:29 +0900

    Translate nav.ts

--- src/i18n/ja/nav.ts
+++ src/i18n/ja/nav.ts
@@ -1,6 +1,50 @@
 import { NavDictionary } from '../translation-checkers';
 
 export default NavDictionary({
-	startHere: 'セットアップ',
+	startHere: 'スタートガイド',
 	'getting-started': 'はじめに',
+	install: 'インストール',
+	'editor-setup': "エディタのセットアップ",
+	migrate: 'アップグレードガイド',
+	'integrations/integrations': 'Astroで構築',
+	'comparing-astro-vs-other-tools': 'Astro vs. X',
+
+	// Core Concepts
+	coreConcepts: 'コアコンセプト',
+	'core-concepts/partial-hydration': 'パーシャルハイドレーション',
+
+	// Basics
+	basics: '基本',
+	'core-concepts/project-structure': 'ディレクトリ構造',
+	'core-concepts/astro-components': 'コンポーネント',
+	'core-concepts/astro-pages': 'ページ',
+	'core-concepts/layouts': 'レイアウト',
+	'guides/markdown-content': 'Markdown',
+	'guides/imports': '静的アセット',
+
+	// Features
+	features: '特徴',
+	'guides/configuring-astro': 'Astroの設定',
+	'guides/styling': 'CSSとスタイル',
+	'guides/debugging': 'デバッグ',
+	'guides/data-fetching': 'データフェッチ',
+	'guides/deploy': 'デプロイ',
+	'guides/environment-variables': "環境変数",
+	'guides/aliases': "importエイリアス",
+	'guides/integrations-guide': 'インテグレーション',
+	'guides/rss': 'RSS',
+	'guides/server-side-rendering': 'サーバーサイドレンダリング（SSR）',
+	'guides/typescript': 'TypeScript',
+	'core-concepts/framework-components': 'UIフレームワーク',
+
+	// Reference
+	reference: 'リファレンス',
+	'reference/configuration-reference': '設定方法',
+	'reference/cli-reference': 'CLI',
+	'reference/api-reference': 'ランタイムAPI',
+	'reference/integrations-reference': 'インテグレーションAPI',
+	'reference/adapter-reference': 'アダプターAPI',
+	'core-concepts/routing': 'ルーティングルール',
+	'reference/directives-reference': 'テンプレートディレクティブ',
+	'guides/publish-to-npm': 'npmパッケージフォーマット',
 });
