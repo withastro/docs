596df2a5e604fc6485fac72708a9dd900d0905f4
Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>
2022-05-09 05:52:48 -0300

    i18n(pt-BR): Adding `editor-setup.md` & `integrations.md` translations + `ui.ts` update (#478)
    * i18n(pt-BR): Adding `editor-setup.md` & `integrations.md` translations
    
    * Update `ui.ts`
    
    * Update src/pages/pt-BR/editor-setup.md
    
    Co-authored-by: Caio Ferrarezi <caioferrarezi2@gmail.com>
    
    * Update src/pages/pt-BR/integrations/integrations.md
    
    Co-authored-by: Caio Ferrarezi <caioferrarezi2@gmail.com>
    
    Co-authored-by: Caio Ferrarezi <caioferrarezi2@gmail.com>

--- src/i18n/pt-BR/nav.ts
+++ src/i18n/pt-BR/nav.ts
@@ -2,11 +2,13 @@ import { NavDictionary } from '../translation-checkers';
 
 export default NavDictionary({
 	startHere: 'Comece Aqui',
-	'getting-started': 'Introdução',
-	install: 'Instalação',
+  'getting-started': 'Introdução',
+  install: 'Instalação',
+  'editor-setup': 'Configuração do Editor',
+  'integrations/integrations': 'Feito com Astro',
 	coreConcepts: 'Principais Conceitos',
 	'core-concepts/partial-hydration': 'Hidratação Parcial',
 	basics: 'Fundamentos',
 	'core-concepts/project-structure': 'Estrutura de Projetos',
-	features: 'Funcionalidades',
+	features: 'Funcionalidades'
 });
