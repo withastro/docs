7d8ccaed335cc7bca4eb7a32011980dbbc68480c
Chris Swithinbank <swithinbank@gmail.com>
2022-05-14 18:01:07 +0200

    Apply @kecrily’s suggestions for nav, ui & install/auto
    Co-authored-by: Percy Ma <kecrily@gmail.com>

--- src/i18n/zh-CN/ui.ts
+++ src/i18n/zh-CN/ui.ts
@@ -5,7 +5,7 @@ export default UIDictionary({
 	'navbar.a11yTitle': '导航顶部',
     // Site settings
 	'site.title': 'Astro 文档',
-	'site.description': '使用较少的客户端javascript来构建更快的网站。',
+	'site.description': '用更少的客户端 JavaScript 构建更快的站点。',
 	'site.og.imageSrc': '/default-og-image.png?v=1',
 	'site.og.imageAlt': '星辰浩瀚的太空中的 astro 标志，右侧前景中漂浮着一颗紫色的土星状行星。',
 	// Left Sidebar
@@ -15,29 +15,30 @@ export default UIDictionary({
 	'leftSidebar.noTranslations': '没有找到翻译内容',
 	'leftSidebar.viewInEnglish': '查看英文版本',
 	// Right Sidebar
-	'rightSidebar.a11yTitle': '内容表',
+	'rightSidebar.a11yTitle': '目录',
 	'rightSidebar.onThisPage': '在本页内',
 	'rightSidebar.overview': '概述',
 	'rightSidebar.more': '更多',
 	'rightSidebar.editPage': '编辑本页内容',
 	'rightSidebar.translatePage': '翻译本页内容',
-	'rightSidebar.joinCommunity': '加入我们的讨论',
+	'rightSidebar.joinCommunity': '加入社群',
+	'rightSidebar.github': 'GitHub 上的 Astro 文档',
 	// Used in previous/next page links at the bottom of pages
 	'articleNav.nextPage': '下一页',
 	'articleNav.prevPage': '返回',
 	// Used in `<Since>`: Added in: v0.24.0 [NEW]
-	'since.addedIn': '增加到：',
-	'since.new': '新的',
+	'since.addedIn': '增加于：',
+	'since.new': '新',
 	// Installation Guide
-	'install.autoTab': '自动命令行',
+	'install.autoTab': '自动化命令行',
 	'install.manualTab': '手动设置',
 	// `<ContributorList>` fallback text
 	'contributors.seeAll': '查看所有贡献者',
 	// Fallback content notice shown when a page is not yet translated
-	'fallbackContent.notice': '该页暂不支持你的语言,我们将为你展示英文页面. 你可以协助我们翻译该页面！',
-	'fallbackContent.linkText': '学习怎样去贡献',
+	'fallbackContent.notice': '此页暂不支持你的语言，我们将为你展示英文页面。你可以帮忙翻译它！',
+	'fallbackContent.linkText': '了解更多关于贡献的内容',
 	// 404 Page
-	'404.title': '没有找到该内容',
-	'404.content': '这个页面不在我们的系统内！',
-	'404.linkText': '拥有我。',
+	'404.title': '未能找到',
+	'404.content': '该页面不在我们的星系中。',
+	'404.linkText': '返回主页',
 });
