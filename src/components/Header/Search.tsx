f32b9e24e077a56718d13064da29a19350e304af
hippotastic <6137925+hippotastic@users.noreply.github.com>
2022-05-24 01:24:06 +0200

    Improve button accessibility on touch devices

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -45,7 +45,7 @@ export default function Search({ lang = 'en', labels }: Props) {
 
 	return (
 		<>
-			<button className="header-button search-input" type="button" ref={searchButtonRef} onClick={onOpen}>
+			<button className="header-button search-input" type="button" aria-label={labels.button} ref={searchButtonRef} onClick={onOpen}>
 				<svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" focusable="false" aria-hidden="true" role="img">
 					<path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" transform="translate(-1)" />
 				</svg>
