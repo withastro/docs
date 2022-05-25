7f4c75d8c8e4b7264a31024a593225ba7902539d
Sarah Rainsberger <sarah@rainsberger.ca>
2022-05-25 16:52:01 -0300

    Routing above Static Assets

--- src/i18n/en/nav.ts
+++ src/i18n/en/nav.ts
@@ -27,8 +27,9 @@ export default [
 	{ text: 'Pages', slug: 'core-concepts/astro-pages', key: 'core-concepts/astro-pages' },
 	{ text: 'Layouts', slug: 'core-concepts/layouts', key: 'core-concepts/layouts' },
 	{ text: 'Markdown', slug: 'guides/markdown-content', key: 'guides/markdown-content' },
-	{ text: 'Static Assets', slug: 'guides/imports', key: 'guides/imports' },
 	{ text: 'Routing', slug: 'core-concepts/routing', key: 'core-concepts/routing' },
+	{ text: 'Static Assets', slug: 'guides/imports', key: 'guides/imports' },
+
 
 	{ text: 'Features', header: true, type: 'learn', key: 'features' },
 	{ text: 'Configuring Astro', slug: 'guides/configuring-astro', key: 'guides/configuring-astro' },
