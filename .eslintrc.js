5dc515ced4eb41850db9978d29f47cc5d1da128c
yosuke ota <otameshiyo23@gmail.com>
2022-06-02 09:11:17 +0900

    Add parser configuration for astro

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,15 @@ module.exports = {
   rules: {},
   overrides: [
     {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {},
+    },
+    {
       files: ['*.ts'],
       parser: '@typescript-eslint/parser',
       extends: ['plugin:@typescript-eslint/recommended'],
