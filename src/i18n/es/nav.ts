5abb74cd0717ad9d6624f7e39f927f0a36646936
Kevin Zuniga Cuellar <46791833+kevinzunigacuellar@users.noreply.github.com>
2022-06-08 13:06:44 -0400

    i18n(es): add `guides/imports.md` (#736)
    * add guides-imports
    
    * fix typos
    
    * fix more typos

--- src/i18n/es/nav.ts
+++ src/i18n/es/nav.ts
@@ -18,6 +18,7 @@ export default NavDictionary({
 	'guides/configuring-astro': 'Configuración de Astro',
 	'guides/data-fetching': 'Obtención de datos',
 	'guides/aliases': 'Aliases de importación',
+	'guides/imports': 'Recursos estáticos',
 	'guides/environment-variables': 'Variables de entorno',
 	'core-concepts/layouts': 'Plantillas de página',
 });
