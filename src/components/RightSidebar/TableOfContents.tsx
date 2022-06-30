df6df41dfbcaff88bfad9af521c7cb8ba176d2a1
delucis <swithinbank@gmail.com>
2022-07-01 01:10:00 +0200

    Button + menu styling

--- src/components/RightSidebar/TableOfContents.tsx
+++ src/components/RightSidebar/TableOfContents.tsx
@@ -33,11 +33,13 @@ const TableOfContents: FunctionalComponent<Props> = ({ headers = [], labels, isM
 		const currentHeading = headers.find(({ slug }) => slug === currentID);
 		return isMobile ? (
 			<summary class="toc-mobile-header">
-				{children}
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 1 16 16" width="16" height="16" aria-hidden="true">
-					<path fill-rule="evenodd" d="M6.22 3.22a.75.75 0 011.06 0l4.25 4.25a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06-1.06L9.94 8 6.22 4.28a.75.75 0 010-1.06z"></path>
-				</svg>
-				{!open && currentHeading?.slug !== 'overview' && <span>{unescapeHtml(currentHeading?.text || '')}</span>}
+				<div className="toc-toggle">
+					{children}
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 1 16 16" width="16" height="16" aria-hidden="true">
+						<path fill-rule="evenodd" d="M6.22 3.22a.75.75 0 011.06 0l4.25 4.25a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06-1.06L9.94 8 6.22 4.28a.75.75 0 010-1.06z"></path>
+					</svg>
+				</div>
+				{!open && currentHeading?.slug !== 'overview' && <span class="toc-current-heading">{unescapeHtml(currentHeading?.text || '')}</span>}
 			</summary>
 		) : (
 			children
