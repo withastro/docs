3c3ec3936e46c3352e2a6d68e2547d407b92f8e8
delucis <swithinbank@gmail.com>
2022-06-30 12:19:54 +0200

    Use classes with Preact to scope styles
    Styles were leaking into other svg and details/summary elements

--- src/components/RightSidebar/TableOfContents.tsx
+++ src/components/RightSidebar/TableOfContents.tsx
@@ -18,14 +18,14 @@ const TableOfContents: FunctionalComponent<Props> = ({ headers = [], labels, isM
 	const [currentID, setCurrentID] = useState('overview');
 
 	const Container = ({ children }) => {
-		return <>{isMobile ? <details>{children}</details> : <>{children}</>}</>;
+		return <>{isMobile ? <details class="toc-mobile-container">{children}</details> : <>{children}</>}</>;
 	};
 	
 	const HeadingContainer = ({children}) => {
 		return (
 			<>
 			{isMobile 
-				? <summary>
+				? <summary class="toc-mobile-header">
 						{children} 
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 1 16 16" width="16" height="16" aria-hidden="true">
 							<path fill-rule="evenodd" d="M6.22 3.22a.75.75 0 011.06 0l4.25 4.25a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06-1.06L9.94 8 6.22 4.28a.75.75 0 010-1.06z"></path>
