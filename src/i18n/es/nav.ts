d0c33de359fb24f972d6807f1da84fd376c2d02a
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-07-01 22:31:29 -0400

    add styling page

--- src/i18n/es/nav.ts
+++ src/i18n/es/nav.ts
@@ -27,4 +27,5 @@ export default NavDictionary({
 	'guides/typescript': 'TypeScript',
 	'guides/server-side-rendering': 'Renderizado en el servidor',
 	'guides/rss': 'RSS',
+	'guides/styling': 'Estilos & CSS',
 });
