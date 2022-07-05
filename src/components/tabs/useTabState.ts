509dc5f2cde15f8fce8471599ee73eb4f10d5f0e
bholmesdev <hey@bholmes.dev>
2022-07-05 16:39:05 -0400

    fix: simplify tab state to fix client:visible

--- src/components/tabs/useTabState.ts
+++ src/components/tabs/useTabState.ts
@@ -1,33 +1,21 @@
-import { useState, useEffect } from 'preact/hooks';
+import { useState } from 'preact/hooks';
 import { useStore } from '@nanostores/preact';
 import { tabStore } from './store';
 
 export function useTabState(initialCurr: string, storeKey?: string): [string, (curr: string) => void] {
-	const $tabStore = useStore(tabStore)
-	// Why use a local state variable alongside the shared store?
-	// This ensures a nice SSR result, without waiting for the store to get set on the client!
-	const [curr, setCurr] = useState<string>(initialCurr)
+	const $tabStore = useStore(tabStore);
+	// Use localState when no storeKey is provided
+	// Would be nice to conditionally create this but alas...
+	// hooks can't use conditionals :(
+	const localState = useState(initialCurr);
+	if (!storeKey) return localState;
 
-	// If you're using a storeKey, set the shared store to an initial value
-	useEffect(() => {
-		if (storeKey) {
-			tabStore.setKey(storeKey, { curr: initialCurr })
-		}
-	}, [storeKey, initialCurr])
-
-	// If you're using a storeKey, update local state whenever the shared store changes
-	useEffect(() => {
-		if (storeKey && $tabStore[storeKey]?.curr) {
-			setCurr($tabStore[storeKey]?.curr)
-		}
-	}, [$tabStore])
-
-	function setCurrStore(newCurr: string) {
-		setCurr(newCurr)
+	const curr = $tabStore[storeKey]?.curr ?? initialCurr;
+	function setCurr(newCurr) {
 		if (storeKey) {
 			tabStore.setKey(storeKey, { curr: newCurr })
 		}
 	}
 
-	return [curr, setCurrStore]
+	return [curr, setCurr]
 }
