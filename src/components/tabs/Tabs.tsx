44d794031132688bb43befefe8d393f6bcaca5d3
bholmesdev <hey@bholmes.dev>
2022-07-05 21:06:07 -0400

    refactor: scroll tab into view when shared store is used

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -1,5 +1,6 @@
-import { useRef, useState } from 'preact/hooks'
-import styles from './Tabs.module.css'
+import type { ComponentChildren } from 'preact';
+import { useRef } from 'preact/hooks';
+import styles from './Tabs.module.css';
 import { useTabState } from './useTabState';
 
 const tabSlotKey = 'tab.' as const;
@@ -8,12 +9,12 @@ const panelSlotKey = 'panel.' as const;
 type TabSlot = `${typeof tabSlotKey}${string}`;
 type PanelSlot = `${typeof panelSlotKey}${string}`;
 
-function isTabSlotEntry(entry: [string, JSX.Element]): entry is [TabSlot, JSX.Element] {
+function isTabSlotEntry(entry: [string, ComponentChildren]): entry is [TabSlot, ComponentChildren] {
 	const [key] = entry;
 	return key.startsWith(tabSlotKey);
 }
 
-function isPanelSlotEntry(entry: [string, JSX.Element]): entry is [PanelSlot, JSX.Element] {
+function isPanelSlotEntry(entry: [string, ComponentChildren]): entry is [PanelSlot, ComponentChildren] {
 	const [key] = entry;
 	return key.startsWith(panelSlotKey);
 }
@@ -27,7 +28,7 @@ function getBaseKeyFromPanel(slot: PanelSlot) {
 }
 
 type Props = {
-	[key: TabSlot | PanelSlot]: JSX.Element;
+	[key: TabSlot | PanelSlot]: ComponentChildren;
 	sharedStore?: string;
 }
 
@@ -36,16 +37,28 @@ export default function Tabs({ sharedStore, ...slots }: Props) {
 	const panels = Object.entries(slots).filter(isPanelSlotEntry)
 	/** Used to focus next and previous tab on arrow key press */
 	const tabButtonRefs = useRef<Record<TabSlot, HTMLButtonElement | null>>({})
+	const scrollRef = useRef<HTMLDivElement | null>(null)
 
 	const firstPanelKey = panels[0]?.[0] ?? ''
 	const [curr, setCurr] = useTabState(getBaseKeyFromPanel(firstPanelKey), sharedStore)
 
-	function moveFocus(event: React.KeyboardEvent<HTMLDivElement>) {
+	function updateCurr(tabSlot: TabSlot) {
+		if (sharedStore) {
+			// Prevents scroll jumping due to layout shift
+			// from other tab views with the same store
+			setTimeout(() => {
+				scrollRef.current?.scrollIntoView()
+			}, 100)
+		}
+		setCurr(getBaseKeyFromTab(tabSlot))
+	}
+
+	function moveFocus(event: KeyboardEvent) {
 		if (event.key === 'ArrowLeft') {
 			const currIdx = tabs.findIndex(([key]) => getBaseKeyFromTab(key) === curr)
 			if (currIdx > 0) {
 				const [prevTabKey] = tabs[currIdx - 1]
-				setCurr(getBaseKeyFromTab(prevTabKey))
+				updateCurr(prevTabKey)
 				tabButtonRefs.current[prevTabKey]?.focus()
 			}
 		}
@@ -53,19 +66,21 @@ export default function Tabs({ sharedStore, ...slots }: Props) {
 			const currIdx = tabs.findIndex(([key]) => getBaseKeyFromTab(key) === curr)
 			if (currIdx < tabs.length - 1) {
 				const [nextTabKey] = tabs[currIdx + 1]
-				setCurr(getBaseKeyFromTab(nextTabKey))
+				updateCurr(nextTabKey)
 				tabButtonRefs.current[nextTabKey]?.focus()
 			}
 		}
 	}
 
 	return (
+		<>
+		<div ref={scrollRef}></div>
 		<div className={styles.container}>
 			<div className={styles.tablist} role="tablist" onKeyDown={moveFocus}>
 				{tabs.map(([key, content]) => (
 					<button
 						ref={el => tabButtonRefs.current[key] = el}
-						onClick={() => setCurr(getBaseKeyFromTab(key))}
+						onClick={() => updateCurr(key)}
 						aria-selected={curr === getBaseKeyFromTab(key)}
 						tabIndex={curr === getBaseKeyFromTab(key) ? 0 : -1}
 						role="tab"
@@ -90,5 +105,6 @@ export default function Tabs({ sharedStore, ...slots }: Props) {
 				</div>
 			))}
 		</div>
+		</>
 	)
 }
