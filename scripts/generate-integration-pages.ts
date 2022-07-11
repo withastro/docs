2c93d8ff0615ac11a6ddcc0b009636c166530fe0
Chris Swithinbank <swithinbank@gmail.com>
2022-07-11 21:10:12 +0200

    Add official integration READMEs to docs (#954)
    * Refactor: move `githubGet` helper for re-use
    
    * Initial sketch of script to generate pages
    
    * Add correct base to any relative links
    
    * Refactor
    
    * Add CI job to update READMEs nightly
    
    * Add some logging to CLI
    
    * Use warnings filter when running script
    
    * Move docs link base stripping to a remark plugin
    
    * Use GitHub as README source
    
    Using npm means waiting for updates to be published — this allows to always have the latest (including my most recent fixes)
    
    * Initial commit of auto-generated integrations pages
    
    * Add bare minimum links from integrations guide
    
    * Exclude deprecated integrations from output
    
    * Use package.json keywords to generate category
    
    * Improve header warning in generated frontmatter
    
    Co-Authored-By: Sarah Rainsberger <sarah@rainsberger.ca>
    
    * Group integrations nav by category
    
    * Handle definition style links in remark plugins
    
    * Make integration nav links canonical
    
    * Move `<video>` styles to global CSS
    
    * Map GitHub video URLs to `<video>` elements
    
    * Refactor `<DeployGuidesNav>` for easier re-use
    
    * Move filtering logic entirely to `DeployGuidesNav`
    
    * Move filter block alignment out of CardsNav
    
    * Use `set:html` for card titles
    
    * Use card nav for integrations nav
    
    * Strip ToCs from integration READMEs
    
    * Use custom layout & remove emojis from titles
    
    * Move vocabulary to ui.ts
    
    * Fix lint error
    
    * Pretty `<details>` 💅
    
    * Fix final item in stack vertical spacing
    
    * Support specifying source branch when generating pages
    
    Helpful for testing changes to the READMEs before committing them
    
    * Move pages to fix sidebar highlighting
    
    * Simplify page title logic
    
    * Add package info to integration page headers
    
    * Tweak IntegrationsNav vertical spacing
    
    * Shorter integrations nav title
    
    * Move package info above fallback content notice
    
    * Accommodate nested `<details>`… 😬
    
    * Use accent colour for `<summary>` marker
    
    * Rerun page generator against updated READMEs
    
    Run against https://github.com/withastro/astro/tree/delucis/integration-readmes
    
    * Style `@astrojs/` differently in integration page titles
    
    * Rerun page generator against updated READMEs
    
    Run against https://github.com/withastro/astro/tree/delucis/integration-readmes
    
    * Stomp on another `<br>`
    
    * Take a pass at restructuring the integrations guide
    
    * updated anchor link to one that still exists
    
    * Fix link
    
    * Fix CI files path
    
    * Expose GITHUB_TOKEN to nightly CI task
    
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>

---
+++ scripts/generate-integration-pages.ts
@@ -0,0 +1,177 @@
+import kleur from 'kleur';
+import type { Definition, HTML, Link, Root, Text } from 'mdast';
+import fetch from 'node-fetch';
+import fs from 'node:fs';
+import { remark } from 'remark';
+import { remove } from 'unist-util-remove';
+import { visit } from 'unist-util-visit';
+import { githubGet } from './lib/github-get.mjs';
+import output from './lib/output.mjs';
+
+interface IntegrationData {
+	name: string;
+	category: 'renderer' | 'adapter' | 'other';
+	readme: string;
+	srcdir: string;
+	version: string;
+}
+
+class IntegrationPagesBuilder {
+	readonly #githubToken?: string;
+	readonly #sourceBranch: string;
+	readonly #sourceRepo: string;
+	readonly #deprecatedIntegrations = new Set(['turbolinks']);
+
+	constructor(opts: { githubToken?: string; sourceBranch: string; sourceRepo: string }) {
+		this.#githubToken = opts.githubToken;
+		this.#sourceBranch = opts.sourceBranch;
+		this.#sourceRepo = opts.sourceRepo;
+
+		if (!this.#githubToken) {
+			if (output.isCi) {
+				output.error('Missing GITHUB_TOKEN. Please add the following lines to the task:\n' + '    env:\n' + '      GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}');
+				process.exit(1);
+			} else {
+				output.warning('You have not set the GITHUB_TOKEN environment variable. ' + 'Calls to Github’s API may hit rate limits without it.');
+			}
+		}
+	}
+
+	/**
+	 * Collect data for each official Astro integration.
+	 */
+	async #getIntegrationData(): Promise<IntegrationData[]> {
+		// Read all the packages in Astro’s integrations directory.
+		const url = `https://api.github.com/repos/${this.#sourceRepo}/contents/packages/integrations?ref=${this.#sourceBranch}`;
+		const packages: { name: string }[] = await githubGet({ url, githubToken: this.#githubToken });
+
+		return await Promise.all(
+			packages
+				.filter((pkg) => !this.#deprecatedIntegrations.has(pkg.name))
+				.map(async (pkg) => {
+					const pkgJsonURL = `https://raw.githubusercontent.com/${this.#sourceRepo}/${this.#sourceBranch}/packages/integrations/${pkg.name}/package.json`;
+					const readmeURL = `https://raw.githubusercontent.com/${this.#sourceRepo}/${this.#sourceBranch}/packages/integrations/${pkg.name}/README.md`;
+					const { name, keywords, version } = await githubGet({ url: pkgJsonURL, githubToken: this.#githubToken });
+					const category = keywords.includes('renderer') ? 'renderer' : keywords.includes('astro-adapter') ? 'adapter' : 'other';
+					const readme = await (await fetch(readmeURL)).text();
+					return { name, category, readme, srcdir: pkg.name, version };
+				})
+		);
+	}
+
+	/**
+	 * Process the raw README markdown:
+	 * - Add frontmatter including a layout
+	 * - Move the README title into frontmatter
+	 * - Add the correct base to any relative links
+	 * - _Remove_ the base from any docs links
+	 */
+	async #processReadme({ name, readme, srcdir, category, version }: IntegrationData): Promise<string> {
+		// Remove title from body
+		readme = readme.replace(/# (.+)/, '');
+		const githubLink = `https://github.com/${this.#sourceRepo}/tree/${this.#sourceBranch}/packages/integrations/${srcdir}/`;
+		const processor = remark()
+			.use(removeTOC)
+			.use(absoluteLinks, { base: githubLink })
+			.use(relativeLinks, { base: `https://docs.astro.build/` })
+			.use(githubVideos);
+		readme = (await processor.process(readme)).toString();
+		readme =
+			`---
+# NOTE: This file is auto-generated from 'scripts/generate-integration-pages.ts'
+#       and pulls content directly from the package’s README.
+#       DO NOT MAKE EDITS TO THIS FILE DIRECTLY, THEY WILL BE OVERWRITTEN!
+#       For corrections, please edit the package README at
+#       ${githubLink}
+
+layout: ~/layouts/IntegrationLayout.astro
+title: '${name}'
+version: '${version}'
+githubURL: '${githubLink}'
+category: ${category}
+i18nReady: false
+---\n\n` + readme;
+		return readme;
+	}
+
+	async #writeReadme(packageName: string, readme: string): Promise<void> {
+		const unscopedName = packageName.split('/').pop();
+		return await fs.promises.writeFile(`src/pages/en/guides/integrations-guide/${unscopedName}.md`, readme, 'utf8');
+	}
+
+	async run() {
+		console.log(kleur.bold('Creating integration pages...\n'));
+		const integrations = await this.#getIntegrationData();
+		await Promise.all(
+			integrations.map(async (integration) => {
+				const readme = await this.#processReadme(integration);
+				await this.#writeReadme(integration.name, readme);
+				console.log('', kleur.green('✔︎'), integration.name);
+			})
+		);
+		console.log('\n');
+	}
+}
+
+const builder = new IntegrationPagesBuilder({
+	sourceBranch: process.env.SOURCE_BRANCH || 'main',
+	sourceRepo: process.env.SOURCE_REPO || 'withastro/astro',
+	githubToken: process.env.GITHUB_TOKEN,
+});
+builder.run();
+
+/** Remark plugin to prepend an absolute path in front of link hrefs. */
+function absoluteLinks({ base }: { base: string }) {
+	return function transform(tree: Root) {
+		function visitor(node: Link | Definition) {
+			// Sanitize URL by removing leading `/`
+			const relativeUrl = node.url.replace(/^.?\//, '');
+			node.url = new URL(relativeUrl, base).href;
+		}
+		visit(tree, 'link', visitor);
+		visit(tree, 'definition', visitor);
+		visit(tree, 'html', function htmlVisitor(node) {
+			node.value = node.value.replace(/(?<=href=")(?!https?:\/\/)\/?(.+)(?=")/g, `${base}$1`);
+		});
+	};
+}
+
+/** Remark plugin to strip the docs base from absolute link hrefs. */
+function relativeLinks({ base }: { base: string }) {
+	return function transform(tree: Root) {
+		function visitor(node: Link | Definition) {
+			if (!node.url.startsWith(base)) return;
+			node.url = new URL(node.url).pathname;
+		}
+		visit(tree, 'link', visitor);
+		visit(tree, 'definition', visitor);
+		visit(tree, 'html', function htmlVisitor(node) {
+			node.value = node.value.replace(new RegExp(base, 'g'), '/');
+		});
+	};
+}
+
+/** Remark plugin to convert GitHub video URLs to `<video>` elements. */
+function githubVideos() {
+	return function transform(tree: Root) {
+		visit(tree, 'text', function visitor(node: Text | HTML) {
+			if (node.value.startsWith('https://user-images.githubusercontent.com/')) {
+				const type = node.value.split('.').pop();
+				node.type = 'html';
+				node.value = `<video controls><source src="${node.value}" type="video/${type}" /></video>`;
+			}
+		});
+	};
+}
+
+/** Remark plugin to strip out the table of contents from an integration README. */
+function removeTOC() {
+	return function transform(tree: Root) {
+		remove(tree, (node) => {
+			if (node.type !== 'list') return;
+			const firstItemContent = node.children[0].children[0];
+			if (firstItemContent.type !== 'paragraph') return;
+			return firstItemContent.children.some((child) => child.type === 'link' && child.url.startsWith('#why'));
+		});
+	};
+}
---
+++ src/pages/en/guides/integrations-guide/deno.md
@@ -0,0 +1,149 @@
+---
+# NOTE: This file is auto-generated from 'scripts/generate-integration-pages.ts'
+#       and pulls content directly from the package’s README.
+#       DO NOT MAKE EDITS TO THIS FILE DIRECTLY, THEY WILL BE OVERWRITTEN!
+#       For corrections, please edit the package README at
+#       https://github.com/withastro/astro/tree/main/packages/integrations/deno/
+
+layout: ~/layouts/IntegrationLayout.astro
+title: '@astrojs/deno'
+version: '0.1.9'
+githubURL: 'https://github.com/withastro/astro/tree/main/packages/integrations/deno/'
+category: adapter
+i18nReady: false
+---
+
+This adapter allows Astro to deploy your SSR site to Deno targets.
+
+## Why Astro Deno
+
+If you're using Astro as a static site builder—its behavior out of the box—you don't need an adapter.
+
+If you wish to [use server-side rendering (SSR)](/en/guides/server-side-rendering/), Astro requires an adapter that matches your deployment runtime.
+
+[Deno](https://deno.land/) is a runtime similar to Node, but with an API that's more similar to the browser's API. This adapter provides access to Deno's API and creates a script to run your project on a Deno server.
+
+## Installation
+
+First, install the `@astrojs/deno` package using your package manager. If you're using npm or aren't sure, run this in the terminal:
+
+```shell
+npm install @astrojs/deno
+```
+
+Then, install this adapter in your `astro.config.*` file using the `adapter` property:
+
+**`astro.config.mjs`**
+
+```js
+import { defineConfig } from 'astro/config';
+import deno from '@astrojs/deno';
+
+export default defineConfig({
+  // ...
+  adapter: deno()
+});
+```
+
+## Usage
+
+After [performing a build](/en/guides/deploy/) there will be a `dist/server/entry.mjs` module. You can start a server by importing this module in your Deno app:
+
+```js
+import './dist/entry.mjs';
+```
+
+See the `start` option below for how you can have more control over starting the Astro server.
+
+You can also run the script directly using deno:
+
+```shell
+deno run --allow-net --allow-read --allow-env ./dist/server/entry.mjs
+```
+
+## Configuration
+
+To configure this adapter, pass an object to the `deno()` function call in `astro.config.mjs`.
+
+**`astro.config.mjs`**
+
+```js
+import { defineConfig } from 'astro/config';
+import deno from '@astrojs/deno';
+
+export default defineConfig({
+  adapter: deno({
+    //options go here
+  })
+});
+```
+
+<details>
+  <summary><strong>start</strong></summary>
+
+This adapter automatically starts a server when it is imported. You can turn this off with the `start` option:
+
+```js
+import { defineConfig } from 'astro/config';
+import deno from '@astrojs/deno';
+
+export default defineConfig({
+  adapter: deno({
+    start: false
+  })
+});
+```
+
+If you disable this, you need to write your own Deno web server. Import and call `handle` from the generated entry script to render requests:
+
+```ts
+import { serve } from "https://deno.land/std@0.132.0/http/server.ts";
+import { handle } from './dist/entry.mjs';
+
+serve((req: Request) => {
+  // Check the request, maybe do static file handling here.
+
+  return handle(req);
+});
+```
+
+</details>
+
+<details>
+  <summary><strong>port</strong> and <strong>hostname</strong></summary>
+
+You can set the port (default: `8085`) and hostname (default: `0.0.0.0`) for the deno server to use. If `start` is false, this has no effect; your own server must configure the port and hostname.
+
+```js
+import { defineConfig } from 'astro/config';
+import deno from '@astrojs/deno';
+
+export default defineConfig({
+  adapter: deno({
+    port: 8081,
+    hostname: 'myhost'
+  })
+});
+```
+
+</details>
+
+## Examples
+
+The [Astro Deno](https://github.com/withastro/astro/tree/main/examples/deno) example includes a `preview:deno` command that runs the entry script directly. Run `npm run build` then `npm run preview:deno` to run the production deno server.
+
+## Troubleshooting
+
+For help, check out the `#support-threads` channel on [Discord](https://astro.build/chat). Our friendly Support Squad members are here to help!
+
+You can also check our [Astro Integration Documentation][astro-integration] for more on integrations.
+
+## Contributing
+
+This package is maintained by Astro's Core team. You're welcome to submit an issue or PR!
+
+## Changelog
+
+See [CHANGELOG.md](https://github.com/withastro/astro/tree/main/packages/integrations/deno/CHANGELOG.md) for a history of changes to this integration.
+
+[astro-integration]: /en/guides/integrations-guide/
