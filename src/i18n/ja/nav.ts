53490e8853592c31b61cbc173159f0ee68752251
Kyosuke <kyosuke@users.noreply.github.com>
2022-07-17 19:00:09 +0900

    Update nav.ts

--- src/i18n/ja/nav.ts
+++ src/i18n/ja/nav.ts
@@ -21,6 +21,7 @@ export default NavDictionary({
 	'core-concepts/layouts': 'レイアウト',
 	'guides/markdown-content': 'Markdown',
 	'guides/imports': '静的アセット',
+	'guides/troubleshooting': 'トラブルシューティング',
 
 	// Features
 	features: '特徴',
