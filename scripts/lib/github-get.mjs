7329a19303fc2e3c892e50db73bda353e28c1a2b
delucis <swithinbank@gmail.com>
2022-08-15 19:44:44 +0200

    Add retry logic to fetch

--- scripts/lib/github-get.mjs
+++ scripts/lib/github-get.mjs
@@ -1,25 +1,28 @@
 import fetch from 'node-fetch';
 
+const MAX_RETRIES = 5;
+
 /**
  * Fetch a URL from the GitHub API and resolve its JSON response.
- * @param {{ url: string; githubToken?: string }} options
+ * @param {{ url: string; githubToken?: string; attempt?: number; }} options
  * @returns {Promise<any>}
  */
-export async function githubGet({ url, githubToken = undefined }) {
+export async function githubGet({ url, githubToken = undefined, attempt = 0 }) {
 	const headers = {
 		Accept: 'application/vnd.github.v3+json',
 	};
 	if (githubToken) {
 		headers.Authorization = `token ${githubToken}`;
 	}
-	const response = await fetch(url, {
-		headers,
-	});
-	const json = await response.json();
-
-	if (!response.ok) {
-		throw new Error(`GitHub API call failed: GET "${url}" returned status ${response.status}: ${JSON.stringify(json)}`);
+	try {
+		const response = await fetch(url, { headers });
+		const json = await response.json();
+		if (!response.ok) {
+			throw new Error(`GitHub API call failed: GET "${url}" returned status ${response.status}: ${JSON.stringify(json)}`);
+		}
+		return json;
+	} catch (error) {
+		if (attempt >= MAX_RETRIES) throw error;
+		return await githubGet({ url, githubToken, attempt: attempt + 1 });
 	}
-
-	return json;
 }
