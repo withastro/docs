9de5c7f6beef5ec4793d39a1a9ef8a585cc6e9b0
hippotastic <6137925+hippotastic@users.noreply.github.com>
2022-08-16 13:52:20 +0200

    Use `p-retry` for automatic `githubGet` retries

--- scripts/lib/github-get.mjs
+++ scripts/lib/github-get.mjs
@@ -1,28 +1,29 @@
 import fetch from 'node-fetch';
-
-const MAX_RETRIES = 5;
+import pRetry, { AbortError } from 'p-retry';
 
 /**
  * Fetch a URL from the GitHub API and resolve its JSON response.
- * @param {{ url: string; githubToken?: string; attempt?: number; }} options
+ * @param {{ url: string; githubToken?: string }} options
  * @returns {Promise<any>}
  */
-export async function githubGet({ url, githubToken = undefined, attempt = 0 }) {
-	const headers = {
-		Accept: 'application/vnd.github.v3+json',
-	};
-	if (githubToken) {
-		headers.Authorization = `token ${githubToken}`;
-	}
-	try {
-		const response = await fetch(url, { headers });
-		const json = await response.json();
-		if (!response.ok) {
-			throw new Error(`GitHub API call failed: GET "${url}" returned status ${response.status}: ${JSON.stringify(json)}`);
-		}
-		return json;
-	} catch (error) {
-		if (attempt >= MAX_RETRIES) throw error;
-		return await githubGet({ url, githubToken, attempt: attempt + 1 });
-	}
+export async function githubGet({ url, githubToken = undefined }) {
+	return await pRetry(
+		async () => {
+			const headers = {
+				Accept: 'application/vnd.github.v3+json',
+			};
+			if (githubToken) {
+				headers.Authorization = `token ${githubToken}`;
+			}
+			const response = await fetch(url, { headers });
+			const json = await response.json();
+
+			if (!response.ok) {
+				throw new AbortError(`GitHub API call failed: GET "${url}" returned status ${response.status}: ${JSON.stringify(json)}`);
+			}
+
+			return json;
+		},
+		{ retries: 5 }
+	);
 }
