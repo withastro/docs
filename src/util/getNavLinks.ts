e9823b5744e162df48c8cba25c64e3a23107aa00
delucis <swithinbank@gmail.com>
2022-09-18 00:22:47 +0200

    Add prev/next arrows to tutorial pages

--- src/util/getNavLinks.ts
+++ src/util/getNavLinks.ts
@@ -12,19 +12,29 @@ interface LinkItem {
 	link: string;
 }
 
+interface PreviousAndNext {
+	previous?: LinkItem;
+	next?: LinkItem;
+}
+
 /**
  * This helper looks for the current page in the global navigation object and,
  * if it finds it, returns the pages before and after it to help build links.
  * @param Astro The Astro global
  * @returns `previous` and `next` links if available
  */
-export async function getNavLinks(
-	Astro: Readonly<AstroGlobal>
-): Promise<{ previous?: LinkItem; next?: LinkItem }> {
+export async function getNavLinks(Astro: Readonly<AstroGlobal>): Promise<PreviousAndNext> {
 	const links = (await getNav(Astro)).filter((x) => !('header' in x) && x.slug) as NavItem[];
+	return getPreviousAndNext(links, Astro);
+}
+
+export function getPreviousAndNext(
+	links: NavItem[],
+	Astro: Readonly<AstroGlobal>
+): PreviousAndNext {
 	const index = links.findIndex((x) => Astro.url.pathname.replace(/\/$/, '').endsWith(x.slug));
 	const lang = getLanguageFromURL(Astro.url.pathname);
-	
+
 	const makeLinkItem = ({ text, slug }: NavItem): LinkItem => ({ text, link: `/${lang}/${slug}/` });
 
 	return {
