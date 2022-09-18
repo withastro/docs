41ea6653d78cfa90d96cd41019dbf1440a07f2af
delucis <swithinbank@gmail.com>
2022-09-19 00:42:53 +0200

    Add unit number & name to tutorial pages

--- src/util/getTutorialPages.ts
+++ src/util/getTutorialPages.ts
@@ -2,14 +2,13 @@ import type { MarkdownInstance } from 'astro';
 import path from 'node:path';
 import { groupPagesByLang } from './groupPagesByLang';
 
+type TutorialPage = MarkdownInstance<{ title: string; unitTitle?: string }>;
+
 /** Get a page’s slug, without the language prefix (e.g. `'/en/migrate'` => `'migrate'`). */
 const slugFromUrl = (url: string) => url.split('/').slice(2).join('/');
 
 /** Get a full list of pages for the tutorial in the current language, falling back to English if not available. */
-export function getTutorialPages(
-	allPages: MarkdownInstance<{ title: string; unitTitle?: string }>[],
-	lang: string
-) {
+export function getTutorialPages(allPages: TutorialPage[], lang: string) {
 	const pagesWithSlug = allPages.map((page) => ({ ...page, slug: slugFromUrl(page.url!) }));
 	const pagesByLang = groupPagesByLang(pagesWithSlug);
 	/** Pages */
@@ -34,4 +33,19 @@ export function getTutorialPages(
 			return aPath.name < bPath.name ? -1 : aPath.name > bPath.name ? 1 : 0;
 		});
 	return pages;
+}
+
+/** Turn a flat list of tutorial pages into a hierarchical array of units and lessons. */
+export function getTutorialUnits(tutorialPages: ReturnType<typeof getTutorialPages>) {
+	return tutorialPages.reduce((units, page) => {
+		if (page.frontmatter.unitTitle) {
+			units.push({
+				title: page.frontmatter.unitTitle,
+				lessons: [page],
+			});
+		} else {
+			units.at(-1)?.lessons.push(page);
+		}
+		return units;
+	}, [] as { title: string; lessons: typeof tutorialPages }[]);
 }
