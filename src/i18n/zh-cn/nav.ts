ef83ce7946cf9319a2a6ead609cce9d6bd4bc2e8
Mogeko <zhengjunyi@live.com>
2022-10-21 11:47:50 +0200

    i18n(zh-cn): Delete outdated links & deprecate Astro vs X (#1700)
    Co-authored-by: Chris Swithinbank <swithinbank@gmail.com>

--- src/i18n/zh-cn/nav.ts
+++ src/i18n/zh-cn/nav.ts
@@ -6,8 +6,6 @@ export default NavDictionary({
 	install: '安装',
 	'editor-setup': '编辑器设置',
 	migrate: '迁移指南',
-	'integrations/integrations': '集成',
-	'comparing-astro-vs-other-tools': 'Astro 与其他工具比较',
 	coreConcepts: '核心理念',
 	basics: '基础内容',
 	'concepts/why-astro': '为什么选择Astro',
