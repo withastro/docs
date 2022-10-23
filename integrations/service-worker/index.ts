17260974ced2c3c22dc84de4e5b8355fc132623f
Zade Viggers <zade@viggers.net>
2022-10-24 00:35:07 +1300

    Cache assets & priority caching system

--- integrations/service-worker/index.ts
+++ integrations/service-worker/index.ts
@@ -11,8 +11,15 @@ export function serviceWorker(): AstroIntegration {
 	return {
 		name: 'astro-docs-service-worker',
 		hooks: {
+			'astro:config:setup': async ({ updateConfig }) => {
+				updateConfig({
+					vite: {
+						plugins: [rollupPlugin()],
+					},
+				});
+			},
 			'astro:build:done': async ({ routes, dir }) => {
-				const compiledTemplate = await generateSw(routes, '1');
+				const compiledTemplate = await generateSw(routes, Date.now().toString());
 
 				const outFile = fileURLToPath(new URL('./service-worker.js', dir));
 				await writeFile(outFile, compiledTemplate);
@@ -21,15 +28,59 @@ export function serviceWorker(): AstroIntegration {
 	};
 }
 
+// Important routes to cache
+const importantRoutes: string[] = ['/', '/offline'];
+
+// Exclude all the *.mjs files - one shows up in the list for each page
+const excludeAssetRegEx = /\.mjs$/;
+
+// Things like css files, js files, etc
+const importantAssets: string[] = ['index.css', 'theme.css', 'favicon.ico', 'favicon.svg'];
+
+// Things like fonts
+const unimportantAssets: string[] = [];
+
+function formatFilePathStrings(strs: string[]): string[] {
+	const newStrs: string[] = [];
+	for (const str of strs) {
+		let newStr = str;
+		if (!newStr.startsWith('/')) newStr = '/' + newStr;
+		newStr = `'${newStr}'`;
+		newStrs.push(newStr);
+	}
+	return newStrs;
+}
+
+function rollupPlugin() {
+	return {
+		name: 'astro-docs-service-worker-rollup-plugin',
+		writeBundle: {
+			async handler(_, bundle: { [fileName: string]: unknown }) {
+				Object.keys(bundle)
+					.filter((fileName) => !fileName.match(excludeAssetRegEx))
+					.forEach((fileName) => {
+						if (importantAssets.includes(fileName)) return;
+						else unimportantAssets.push(fileName);
+					});
+			},
+		},
+	};
+}
+
 async function generateSw(routes: RouteData[], hash: string): Promise<string> {
 	const swTemplate = await readFile(join(__dirname, 'service-worker-template.js'), 'utf-8');
 
-	const routePaths = routes
-		.filter((route) => !!route.pathname)
-		.map((route) => `'${route.pathname}'`);
+	const routePaths = routes.filter((route) => !!route.pathname).map((route) => route.pathname!);
+
+	const importantFiles = formatFilePathStrings([...importantRoutes, ...importantAssets]);
+	const otherFiles = formatFilePathStrings([
+		...routePaths.filter((route) => !importantRoutes.includes(route)),
+		...unimportantAssets,
+	]);
 
 	const compiledTemplate = swTemplate
-		.replace('/* $%_routes_%$ */', routePaths.join(','))
+		.replace('/* $%_priority_files_%$ */', importantFiles.join(','))
+		.replace('/* $%_other_files_%$ */', otherFiles.join(','))
 		.replace('/* $%_hash_%$ */', hash);
 
 	return compiledTemplate;
--- integrations/service-worker/service-worker-template.js
+++ integrations/service-worker/service-worker-template.js
@@ -1,38 +1,60 @@
-const astroRoutes = [
-	/* $%_routes_%$ */
-];
-
-const offlinePage = '/offline/';
-
-const cacheFiles = [...astroRoutes, offlinePage];
-
-const CURRENT_CACHE = 'main-' + '/* $%_hash_%$ */';
-
 // Worker based on https://gist.github.com/JMPerez/8ca8d5ffcc0cc45a8b4e1c279efd8a94
 
+const astroPriorityFiles = [
+	/* $%_priority_files_%$ */
+];
+const astroOtherFiles = [
+	/* $%_other_files_%$ */
+];
+
+const offlinePage = '/offline';
+
+const CURRENT_CACHE = 'main-/* $%_hash_%$ */';
+
+function logInfo(...toLog) {
+	console.info('%c[Offline worker]', 'color: #ff5e00', ...toLog);
+}
+
+function clearAllOldCaches() {
+	return caches.keys().then((cacheNames) => {
+		return Promise.all(
+			cacheNames.map((cacheName) => {
+				if (cacheName !== CURRENT_CACHE) {
+					logInfo(`Found old cache ${cacheName}. Removing...`);
+					return caches.delete(cacheName);
+				}
+			})
+		);
+	});
+}
+
 // on activation we clean up the previously registered service workers
-self.addEventListener('activate', (evt) =>
-	evt.waitUntil(
-		caches.keys().then((cacheNames) => {
-			return Promise.all(
-				cacheNames.map((cacheName) => {
-					if (cacheName !== CURRENT_CACHE) {
-						return caches.delete(cacheName);
-					}
-				})
-			);
-		})
-	)
-);
+self.addEventListener('activate', (evt) => {
+	logInfo(`Installing...`);
+
+	evt.waitUntil(clearAllOldCaches());
+});
+
+async function addFiles() {
+	caches.open(CURRENT_CACHE).then(async (cache) => {
+		// The addAll method doesn't take the list order into account,
+		// so we need to call it separately to be able to prioritise certain files
+		await cache.addAll(astroPriorityFiles);
+		logInfo('Priority files downloaded');
+
+		await cache.addAll(astroOtherFiles);
+		logInfo(`All assets downloaded. Ready to work offline.`);
+	});
+}
 
 // on install we download the routes we want to cache for offline
-self.addEventListener('install', (evt) =>
-	evt.waitUntil(
-		caches.open(CURRENT_CACHE).then((cache) => {
-			return cache.addAll(cacheFiles);
-		})
-	)
-);
+self.addEventListener('install', async (evt) => {
+	logInfo(`Installed. Downloading assets...`);
+
+	await evt.waitUntil(addFiles());
+
+	clearAllOldCaches();
+});
 
 // fetch the resource from the network
 const fromNetwork = (request, timeout) =>
@@ -47,9 +69,21 @@ const fromNetwork = (request, timeout) =>
 
 // fetch the resource from the browser cache
 const fromCache = (request) =>
-	caches
-		.open(CURRENT_CACHE)
-		.then((cache) => cache.match(request).then((matching) => matching || cache.match(offlinePage)));
+	caches.open(CURRENT_CACHE).then((cache) =>
+		cache.match(request).then(async (matching) => {
+			const matchingRes = matching || (await cache.match(offlinePage));
+
+			// If both the requested page & the offline page aren't cached, just don't
+			// respond to the request and let the browser show it's offline UI
+			if (!matchingRes) return;
+
+			const modifiedRes = new Response(
+				(await matchingRes.text()) +
+					`<meta data-from-sw-cache="true" data-cache-name="${CURRENT_CACHE}" />`
+			);
+			return modifiedRes;
+		})
+	);
 
 // cache the current page to make it available for offline
 const update = (request) =>
@@ -60,6 +94,6 @@ const update = (request) =>
 // general strategy when making a request (eg if online try to fetch it
 // from the network with a timeout, if something fails serve from cache)
 self.addEventListener('fetch', (evt) => {
-	evt.respondWith(fromNetwork(evt.request, 10000).catch(() => fromCache(evt.request)));
+	evt.respondWith(fromNetwork(evt.request, 3000).catch(() => fromCache(evt.request)));
 	evt.waitUntil(update(evt.request));
 });
