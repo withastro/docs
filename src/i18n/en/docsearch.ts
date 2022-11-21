6f442b86b87c4406a464f7c57b4b5a7bf35cf071
Chris Swithinbank <swithinbank@gmail.com>
2022-11-21 20:41:45 +0100

    Less controversial collective nouns
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>

--- src/i18n/en/docsearch.ts
+++ src/i18n/en/docsearch.ts
@@ -5,8 +5,8 @@ export default DocSearchDictionary({
 	placeholder: 'Search docs',
 	shortcutLabel: 'Press / to search',
 	resultsFooterLede: 'Looking for an Astro integration or theme? Need more help?',
-	resultsFooterIntegrations: 'Astro Integrations catalog',
-	resultsFooterThemes: 'Astro Themes catalog',
+	resultsFooterIntegrations: 'Astro Integrations directory',
+	resultsFooterThemes: 'Astro Themes showcase',
 	resultsFooterDiscord: 'Join us on Discord',
 	modal: {},
 });
