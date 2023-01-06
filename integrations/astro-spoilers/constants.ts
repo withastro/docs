31e664fddfb5eeec9580255002bccd4bd02e400f
Chris Swithinbank <swithinbank@gmail.com>
2023-01-06 15:03:53 +0100

    Remove `||` spoiler syntax extension

--- integrations/astro-spoilers/constants.ts
+++
@@ -1,9 +0,0 @@
-import { codes } from 'micromark-util-symbol/codes';
-
-export const SpoilerTagname = 'AutoImportedSpoiler';
-export const SpoilerMarker = codes.verticalBar;
-export enum SpoilerStages {
-	spoilerText = 'spoilerText',
-	spoilerTextSequence = 'spoilerTextSequence',
-	spoilerTextData = 'spoilerTextData',
-}
