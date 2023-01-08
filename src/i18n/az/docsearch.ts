c654463a41bab6a92c81fda34b57c3ba82b4b526
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-01-08 13:31:59 +0400

    `docsearch`  file for [az] language

---
+++ src/i18n/az/docsearch.ts
@@ -0,0 +1,12 @@
+import { DocSearchDictionary } from '../translation-checkers';
+
+export default DocSearchDictionary({
+	button: 'Axtar',
+	placeholder: 'Dokumentasiyanı axtar',
+	shortcutLabel: 'Axtarmaq üçün / bas',
+	resultsFooterLede: 'Astro üçün tema və inteqrasiya axtarırsan? Köməyə ehtiyacın var?',
+	resultsFooterIntegrations: 'Astro inteqrasiya kataloqu',
+	resultsFooterThemes: 'Astro temaları',
+	resultsFooterDiscord: 'Discordda bizə qatıl',
+	modal: {},
+});
