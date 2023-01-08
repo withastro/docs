b5a5f3423d53899abc5cfd4747f7ec88c9e8a470
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-01-08 13:32:39 +0400

    `ui` file for [az] language

---
+++ src/i18n/az/ui.ts
@@ -0,0 +1,91 @@
+export default {
+	'a11y.skipLink': 'Skip to Content',
+	'a11y.sectionLink': 'Section titled',
+	'navbar.a11yTitle': 'Top',
+	// Site settings
+	'site.title': 'Astro Dokumentasiya',
+	'site.description': 'Build faster websites with less client-side Javascript.',
+	'site.og.imageSrc': '/default-og-image.png?v=1',
+	'site.og.imageAlt':
+		'astro logo on a starry expanse of space, with a purple saturn-like planet floating in the right foreground',
+	// Left Sidebar
+	'leftSidebar.a11yTitle': 'Primary',
+	'leftSidebar.learnTab': 'Öyrən',
+	'leftSidebar.referenceTab': 'İstinad',
+	'leftSidebar.viewInEnglish': 'View in English',
+	'leftSidebar.sponsoredBy': 'Sponsored by',
+	// Right Sidebar
+	'rightSidebar.a11yTitle': 'Secondary',
+	'rightSidebar.onThisPage': 'Bu səhifədə',
+	'rightSidebar.overview': 'İlk baxış',
+	'rightSidebar.community': 'İcma',
+	'rightSidebar.joinDiscord': 'Discordda bizə qatıl',
+	'rightSidebar.readBlog': 'Bloq postlarımız',
+	'rightSidebar.openCollective': 'Bizim Open Collective',
+	'rightSidebar.contribute': 'Töhvə verin',
+	'rightSidebar.editPage': 'Səhifəni redaktə edin',
+	'rightSidebar.translatePage': 'Səhifəni tərcümə edin',
+	'rightSidebar.github': 'Astro Dokumentasiyası Githubda',
+	// Footer
+	'footer.privacyPolicy': 'Privacy Policy',
+	// `<ThemeToggleButton>` acessibility labels
+	'themeToggle.useLight': 'Açıq tema',
+	'themeToggle.useDark': 'Tünd tema',
+	// Used in previous/next page links at the bottom of pages
+	'articleNav.nextPage': 'Sonrakı səhifə',
+	'articleNav.prevPage': 'Geri',
+	// Used in `<Since>`: Added in: v0.24.0 [NEW]
+	'since.addedIn': 'Əlavə edilib',
+	'since.new': 'Yeni',
+	// Installation Guide
+	'install.autoTab': 'Avtomatik CLI',
+	'install.manualTab': 'Manual Setup',
+	// `<DeployGuidesNav>` vocabulary
+	'deploy.sectionTitle': 'Deployment Guides',
+	'deploy.altSectionTitle': 'More Deployment Guides',
+	'deploy.filterLabel': 'Filter by deploy type',
+	'deploy.ssrTag': 'SSR',
+	'deploy.staticTag': 'Static',
+	// CMS Guides vocabulary
+	'cms.navTitle': 'More CMS guides',
+	// Migration Guides vocabulary
+	'migration.navTitle': 'More migration guides',
+	// `<ContributorList>` fallback text
+	'contributors.seeAll': 'See all contributors',
+	// Fallback content notice shown when a page is not yet translated
+	'fallbackContent.notice':
+		'This page is not yet available in your language, so we’re showing you the English version. You can help by translating it!',
+	'fallbackContent.linkText': 'Learn more about how you can contribute',
+	// 404 Page
+	'404.title': 'Tapılmadı',
+	'404.content': 'Bu səhifə bizim Günəş sistemimizdə yoxdur.',
+	'404.linkText': 'Məni geri apar.',
+	// Aside component default labels
+	'aside.note': 'Qeyd',
+	'aside.tip': 'İpucu',
+	'aside.caution': 'Xəbərdarlıq',
+	'aside.danger': 'Ehtiyatlı olun',
+	// Integrations vocabulary
+	'integrations.footerTitle': 'Daha çox İnteqrasiya',
+	'integrations.renderers': 'UI Freymvorkları',
+	'integrations.adapters': 'SSR Adapterləri',
+	'integrations.others': 'Digərləri',
+	// Checklist component
+	'checklist.or': 'or',
+	// Multiple Choice component
+	'multipleChoice.defaultCorrect': 'Doğrudur!',
+	'multipleChoice.defaultIncorrect': 'Yenidən yoxla!',
+	'multipleChoice.submitLabel': 'Göndər',
+	// Tutorial Progress
+	'progress.todo': 'To-do',
+	'progress.done': 'Tamamlanmış',
+	// Tutorial Navigation
+	'tutorial.trackerLabel': 'Tutorial Tracker',
+	'tutorial.unit': 'Fəsil',
+	// Tutorial
+	'tutorial.getReady': 'Hazır olun…',
+	// Feedback Fish widget
+	'feedback.button': 'Give us feedback',
+	// `<FileTree>` component
+	'fileTree.directoryLabel': 'Directory',
+};
