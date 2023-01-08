1ff76001c8e36d903a8c3631d02335282b6a95f4
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-01-08 13:32:26 +0400

    `nav` file for [az] language

---
+++ src/i18n/az/nav.ts
@@ -0,0 +1,56 @@
+import { NavDictionary } from '../translation-checkers';
+
+export default NavDictionary({
+	// Start Here
+	startHere: 'Burdan Başlayın',
+	'getting-started': 'İlk Addımlar',
+	install: 'Yükləmək',
+	'editor-setup': 'Text Redaktorunda Qurmaq',
+	migrate: 'Astroya Keçirmək',
+	
+
+	// Core Concepts
+	coreConcepts: 'Konsepsiya',
+	'concepts/islands': 'Astro Adalar',
+	'concepts/why-astro': 'Niyə Məhz Astro',
+	'concepts/mpa-vs-spa': 'MPA vs. SPA',
+
+	// Basics
+	basics: 'Əsas Konsepsiya',
+	'core-concepts/project-structure': 'Proyektin Strukturu',
+	'core-concepts/astro-components': 'Komponentlər',
+	'core-concepts/astro-pages': 'Səhifələr',
+	'core-concepts/layouts': 'Maketlər (Tərtibatlar)',
+	'guides/markdown-content': 'Markdown',
+	'core-concepts/routing': 'Routinq',
+	'guides/imports': 'İmportlar',
+	'core-concepts/endpoints': 'Endpointlər',
+	'guides/deploy': 'Deploy',
+
+	// Features
+	features: 'Xüsusiyyətlər',
+	'guides/configuring-astro': 'Astro Konfiqurasiya Faylları',
+	'guides/styling': 'Formatlaşdırma və CSS',
+	'guides/data-fetching': 'Fetch',
+	'guides/environment-variables': 'Mühit Dəyişənləri',
+	'guides/aliases': 'İmport üçün Şərti Adlar',
+	'guides/integrations-guide': 'Integrationen',
+	'guides/fonts': 'Şriftlər',
+	'guides/cms': 'CMS Quraşdırılması',
+	'guides/rss': 'RSS',
+	'guides/testing': 'Test-yoxlama',
+	'guides/server-side-rendering': 'Server Tərəfli Renderləmə (SSR)',
+	'guides/typescript': 'TypeScript',
+	'core-concepts/framework-components': 'UI-Freymvorkləri',
+
+	// Reference
+	reference: 'İstinadlar',
+	'reference/configuration-reference': 'Ayarlar',
+	'reference/cli-reference': 'CLI',
+	'reference/api-reference': 'Runtime API',
+	'reference/integrations-reference': 'Inteqrasiya üçün API-lar',
+	'reference/adapter-reference': 'Adapter API-lar',
+	'reference/directives-reference': 'Şablon Direktivləri',
+	'guides/publish-to-npm': 'NPM Paket Formatı',
+
+})
