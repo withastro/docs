da2fd6c03b21551b7a160b26b6f8f4224294e4c4
Shinya Fujino <shf0811@gmail.com>
2023-01-11 11:47:13 +0900

    Update src/i18n/ja/docsearch.ts
    Co-authored-by: Kyosuke <kyosuke@users.noreply.github.com>

--- src/i18n/ja/docsearch.ts
+++ src/i18n/ja/docsearch.ts
@@ -4,7 +4,7 @@ export default DocSearchDictionary({
 	button: '検索',
 	placeholder: 'ドキュメントを検索する',
 	shortcutLabel: '/ キーを押して検索する',
-	resultsFooterLede: 'Astroインテグレーションやテーマをお探しですか？',
+	resultsFooterLede: 'Astroインテグレーションやテーマをお探しですか？ヘルプが必要でしょうか？',
 	resultsFooterIntegrations: 'Astroインテグレーション一覧',
 	resultsFooterThemes: 'Astroテーマのショーケース',
 	resultsFooterDiscord: 'Discordに参加する',
