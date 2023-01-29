10f5cfdf4a3df3d16cf7afcc87a903464f026d3d
Igor Kowalczyk <majonezexe@protonmail.com>
2023-01-29 18:55:17 +0100

    Update src/i18n/pl/docsearch.ts
    Co-authored-by: Jakub Robaszek <112584909+JakubRobaszek@users.noreply.github.com>

--- src/i18n/pl/docsearch.ts
+++ src/i18n/pl/docsearch.ts
@@ -2,7 +2,7 @@ import { DocSearchDictionary } from '../translation-checkers';
 
 export default DocSearchDictionary({
 	button: 'Szukaj',
-	placeholder: 'Wyszuaj w dokumentacji',
+	placeholder: 'Wyszukaj w dokumentacji',
 	shortcutLabel: 'Naciśnij klawisz / aby wyszukać',
 	resultsFooterLede: 'Szukasz integracji lub motywu Astro? Potrzebujesz pomocy?',
 	resultsFooterIntegrations: 'Katalog integracji Astro',
