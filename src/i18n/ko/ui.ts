637f3ffbee02b44a49f9b835b2c20b6b237bd336
imjlk <i@m.junglei.kim>
2023-02-14 22:29:35 +0900

    translate `ui.ts`

--- src/i18n/ko/ui.ts
+++ src/i18n/ko/ui.ts
@@ -1,95 +1,95 @@
 import { UIDictionary } from '../translation-checkers';
 
 export default UIDictionary({
-	'a11y.skipLink': 'Skip to Content',
-	'a11y.sectionLink': 'Section titled',
-	'navbar.a11yTitle': 'Top',
-	// Site settings
-	'site.title': 'Astro Documentation',
-	'site.description': 'Build faster websites with less client-side Javascript.',
+	'a11y.skipLink': '본문 바로가기',
+	'a11y.sectionLink': '섹션 제목',
+	'navbar.a11yTitle': '맨 위로',
+	// Site settings >>>> REVIEW: 리뷰 필요
+	'site.title': 'Astro 문서',
+	'site.description': '클라이언트 사이드 자바스크립트를 줄이고, 더 빠른 웹 사이트를 구축하기.',
 	'site.og.imageSrc': '/default-og-image.png?v=1',
 	'site.og.imageAlt':
-		'astro logo on a starry expanse of space, with a purple saturn-like planet floating in the right foreground',
+		'별이 쏟아지는 광활한 우주에 Astro 로고가 있고 오른쪽 전경에 보라색 토성 모양의 행성이 떠 있습니다.',
 	// Left Sidebar
-	'leftSidebar.a11yTitle': 'Primary',
-	'leftSidebar.learnTab': 'Learn',
-	'leftSidebar.referenceTab': 'Reference',
-	'leftSidebar.viewInEnglish': 'View in English',
-	'leftSidebar.sponsoredBy': 'Sponsored by',
+	'leftSidebar.a11yTitle': '주 메뉴',
+	'leftSidebar.learnTab': '배우기',
+	'leftSidebar.referenceTab': '참고자료',
+	'leftSidebar.viewInEnglish': '영어로 보기',
+	'leftSidebar.sponsoredBy': '스폰서',
 	// Right Sidebar
-	'rightSidebar.a11yTitle': 'Secondary',
-	'rightSidebar.onThisPage': 'On this page',
-	'rightSidebar.overview': 'Overview',
-	'rightSidebar.community': 'Community',
+	'rightSidebar.a11yTitle': '보조 메뉴',
+	'rightSidebar.onThisPage': '목차',
+	'rightSidebar.overview': '개요',
+	'rightSidebar.community': '커뮤니티',
 	'rightSidebar.joinDiscord': 'Join us on Discord',
 	'rightSidebar.readBlog': 'Read our blog posts',
-	'rightSidebar.openCollective': 'Our Open Collective',
-	'rightSidebar.contribute': 'Contribute',
-	'rightSidebar.editPage': 'Edit this page',
-	'rightSidebar.translatePage': 'Translate this page',
-	'rightSidebar.github': 'Astro Docs on GitHub',
+	'rightSidebar.openCollective': 'Open Collective',
+	'rightSidebar.contribute': '기여하기',
+	'rightSidebar.editPage': '이 페이지 편집하기',
+	'rightSidebar.translatePage': '이 페이지 번역하기',
+	'rightSidebar.github': 'GitHub Astro 문서',
 	// Footer
-	'footer.privacyPolicy': 'Privacy Policy',
+	'footer.privacyPolicy': '개인정보보호정책',
 	// `<ThemeToggleButton>` acessibility labels
-	'themeToggle.useLight': 'Use light theme',
-	'themeToggle.useDark': 'Use dark theme',
+	'themeToggle.useLight': '밝은 테마 사용',
+	'themeToggle.useDark': '어두운 테마 사용',
 	// Used in previous/next page links at the bottom of pages
-	'articleNav.nextPage': 'Next Page',
-	'articleNav.prevPage': 'Back',
+	'articleNav.nextPage': '다음 페이지',
+	'articleNav.prevPage': '뒤로',
 	// Used in `<Since>`: Added in: v0.24.0 [NEW]
-	'since.addedIn': 'Added in:',
-	'since.new': 'New',
-	'since.beta': 'Beta',
+	'since.addedIn': '추가됨:',
+	'since.new': '신규',
+	'since.beta': '베타',
 	// Installation Guide
 	'install.autoTab': 'Automatic CLI',
 	'install.manualTab': 'Manual Setup',
 	// `<DeployGuidesNav>` vocabulary
-	'deploy.sectionTitle': 'Deployment Guides',
-	'deploy.altSectionTitle': 'More Deployment Guides',
-	'deploy.filterLabel': 'Filter by deploy type',
+	'deploy.sectionTitle': '배포 가이드',
+	'deploy.altSectionTitle': '배포 가이드 더 보기',
+	'deploy.filterLabel': '배포 유형별 필터',
 	'deploy.ssrTag': 'SSR',
 	'deploy.staticTag': 'Static',
 	// CMS Guides vocabulary
-	'cms.navTitle': 'More CMS guides',
+	'cms.navTitle': 'CMS 가이드 더 보기',
 	// Migration Guides vocabulary
-	'migration.navTitle': 'More migration guides',
+	'migration.navTitle': '마이그레이션 가이드 더 보기',
 	// `<ContributorList>` fallback text
-	'contributors.seeAll': 'See all contributors',
+	'contributors.seeAll': '모든 기여자 보기',
 	// Fallback content notice shown when a page is not yet translated
 	'fallbackContent.notice':
-		'This page is not yet available in your language, so we’re showing you the English version. You can help by translating it!',
-	'fallbackContent.linkText': 'Learn more about how you can contribute',
+		'이 페이지는 아직 한국어로 제공되지 않으므로 영어 버전을 보여드립니다. 번역 활동에 참여해보세요!',
+	'fallbackContent.linkText': '기여 방법에 대해 자세히 알아보기',
 	// 404 Page
-	'404.title': 'Not Found',
-	'404.content': 'This page isn’t in our solar system.',
-	'404.linkText': 'Take me home.',
+	'404.title': '페이지를 찾을 수 없습니다',
+	'404.content': '이 페이지는 우리 태양계에 존재하지 않습니다.',
+	'404.linkText': '기존 차원으로 돌아가기',
 	// Aside component default labels
-	'aside.note': 'Note',
-	'aside.tip': 'Tip',
-	'aside.caution': 'Caution',
-	'aside.danger': 'Danger',
+	'aside.note': '노트',
+	'aside.tip': '팁',
+	'aside.caution': '주의',
+	'aside.danger': '위험',
 	// Integrations vocabulary
-	'integrations.changelog': 'Changelog',
-	'integrations.footerTitle': 'More Integrations',
-	'integrations.renderers': 'UI Frameworks',
-	'integrations.adapters': 'SSR Adapters',
-	'integrations.others': 'Others',
-	// Checklist component
-	'checklist.or': 'or',
-	// Multiple Choice component
-	'multipleChoice.defaultCorrect': 'Correct!',
-	'multipleChoice.defaultIncorrect': 'Try again!',
-	'multipleChoice.submitLabel': 'Submit',
-	// Tutorial Progress
-	'progress.todo': 'To-do',
-	'progress.done': 'Complete',
-	// Tutorial Navigation
-	'tutorial.trackerLabel': 'Tutorial Tracker',
-	'tutorial.unit': 'Unit',
-	// Tutorial
-	'tutorial.getReady': 'Get ready to…',
-	// Feedback Fish widget
-	'feedback.button': 'Give us feedback',
-	// `<FileTree>` component
-	'fileTree.directoryLabel': 'Directory',
+	'integrations.changelog': '변경 로그',
+	'integrations.footerTitle': '더 많은 통합',
+	'integrations.renderers': 'UI 프레임워크',
+	'integrations.adapters': 'SSR 어댑터',
+	'integrations.others': '기타',
+	// Checklist component >>>> TODO: 컨텍스트 확인 못함
+	'checklist.or': '또는',
+	// Multiple Choice component >>>> TODO: 컨텍스트 확인 못함
+	'multipleChoice.defaultCorrect': '정답!',
+	'multipleChoice.defaultIncorrect': '다시 시도해보세요!',
+	'multipleChoice.submitLabel': '제출하기',
+	// Tutorial Progress >>>> TODO: 컨텍스트 확인 못함
+	'progress.todo': 'To do',
+	'progress.done': '완료',
+	// Tutorial Navigation >>>> TODO: 컨텍스트 확인 못함
+	'tutorial.trackerLabel': '튜토리얼 트래커',
+	'tutorial.unit': '단위',
+	// Tutorial >>>> TODO: 컨텍스트 확인 못함
+	'tutorial.getReady': '준비…',
+	// Feedback Fish widget >>>> TODO: 컨텍스트 확인 못함
+	'feedback.button': '의견을 보내주세요',
+	// `<FileTree>` component 
+	'fileTree.directoryLabel': '디렉토리',
 });
