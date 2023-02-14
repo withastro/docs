9a352c15b5e31b16539e2f2256e010bb55c76da6
imjlk <i@m.junglei.kim>
2023-02-14 21:44:49 +0900

    copy from `ui.ts`

--- src/i18n/ko/ui.ts
+++ src/i18n/ko/ui.ts
@@ -1,3 +1,95 @@
 import { UIDictionary } from '../translation-checkers';
 
-export default UIDictionary({});
+export default UIDictionary({
+	'a11y.skipLink': 'Skip to Content',
+	'a11y.sectionLink': 'Section titled',
+	'navbar.a11yTitle': 'Top',
+	// Site settings
+	'site.title': 'Astro Documentation',
+	'site.description': 'Build faster websites with less client-side Javascript.',
+	'site.og.imageSrc': '/default-og-image.png?v=1',
+	'site.og.imageAlt':
+		'astro logo on a starry expanse of space, with a purple saturn-like planet floating in the right foreground',
+	// Left Sidebar
+	'leftSidebar.a11yTitle': 'Primary',
+	'leftSidebar.learnTab': 'Learn',
+	'leftSidebar.referenceTab': 'Reference',
+	'leftSidebar.viewInEnglish': 'View in English',
+	'leftSidebar.sponsoredBy': 'Sponsored by',
+	// Right Sidebar
+	'rightSidebar.a11yTitle': 'Secondary',
+	'rightSidebar.onThisPage': 'On this page',
+	'rightSidebar.overview': 'Overview',
+	'rightSidebar.community': 'Community',
+	'rightSidebar.joinDiscord': 'Join us on Discord',
+	'rightSidebar.readBlog': 'Read our blog posts',
+	'rightSidebar.openCollective': 'Our Open Collective',
+	'rightSidebar.contribute': 'Contribute',
+	'rightSidebar.editPage': 'Edit this page',
+	'rightSidebar.translatePage': 'Translate this page',
+	'rightSidebar.github': 'Astro Docs on GitHub',
+	// Footer
+	'footer.privacyPolicy': 'Privacy Policy',
+	// `<ThemeToggleButton>` acessibility labels
+	'themeToggle.useLight': 'Use light theme',
+	'themeToggle.useDark': 'Use dark theme',
+	// Used in previous/next page links at the bottom of pages
+	'articleNav.nextPage': 'Next Page',
+	'articleNav.prevPage': 'Back',
+	// Used in `<Since>`: Added in: v0.24.0 [NEW]
+	'since.addedIn': 'Added in:',
+	'since.new': 'New',
+	'since.beta': 'Beta',
+	// Installation Guide
+	'install.autoTab': 'Automatic CLI',
+	'install.manualTab': 'Manual Setup',
+	// `<DeployGuidesNav>` vocabulary
+	'deploy.sectionTitle': 'Deployment Guides',
+	'deploy.altSectionTitle': 'More Deployment Guides',
+	'deploy.filterLabel': 'Filter by deploy type',
+	'deploy.ssrTag': 'SSR',
+	'deploy.staticTag': 'Static',
+	// CMS Guides vocabulary
+	'cms.navTitle': 'More CMS guides',
+	// Migration Guides vocabulary
+	'migration.navTitle': 'More migration guides',
+	// `<ContributorList>` fallback text
+	'contributors.seeAll': 'See all contributors',
+	// Fallback content notice shown when a page is not yet translated
+	'fallbackContent.notice':
+		'This page is not yet available in your language, so we’re showing you the English version. You can help by translating it!',
+	'fallbackContent.linkText': 'Learn more about how you can contribute',
+	// 404 Page
+	'404.title': 'Not Found',
+	'404.content': 'This page isn’t in our solar system.',
+	'404.linkText': 'Take me home.',
+	// Aside component default labels
+	'aside.note': 'Note',
+	'aside.tip': 'Tip',
+	'aside.caution': 'Caution',
+	'aside.danger': 'Danger',
+	// Integrations vocabulary
+	'integrations.changelog': 'Changelog',
+	'integrations.footerTitle': 'More Integrations',
+	'integrations.renderers': 'UI Frameworks',
+	'integrations.adapters': 'SSR Adapters',
+	'integrations.others': 'Others',
+	// Checklist component
+	'checklist.or': 'or',
+	// Multiple Choice component
+	'multipleChoice.defaultCorrect': 'Correct!',
+	'multipleChoice.defaultIncorrect': 'Try again!',
+	'multipleChoice.submitLabel': 'Submit',
+	// Tutorial Progress
+	'progress.todo': 'To-do',
+	'progress.done': 'Complete',
+	// Tutorial Navigation
+	'tutorial.trackerLabel': 'Tutorial Tracker',
+	'tutorial.unit': 'Unit',
+	// Tutorial
+	'tutorial.getReady': 'Get ready to…',
+	// Feedback Fish widget
+	'feedback.button': 'Give us feedback',
+	// `<FileTree>` component
+	'fileTree.directoryLabel': 'Directory',
+});
