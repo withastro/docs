af2e870d57888633167e0470184fb78ae32f59f9
Chris Swithinbank <swithinbank@gmail.com>
2023-02-24 15:46:53 +0100

    Fix nav key type issue

--- src/i18n/en/nav.ts
+++ src/i18n/en/nav.ts
@@ -44,7 +44,7 @@ export default [
 	{ text: 'Connect a CMS', slug: 'guides/cms', key: 'guides/cms' },
 	{ text: 'Add integrations', slug: 'guides/integrations-guide', key: 'guides/integrations-guide' },
 	{ text: 'Deploy your site', slug: 'guides/deploy', key: 'guides/deploy' },
-	{ text: 'More Recipes', slug: 'recipes', key: 'recipes' },
+	{ text: 'More Recipes', slug: 'recipes', key: 'guides/recipes' },
 
 
 	// TODO: Can some of these be removed, merged, or pulled entirely into Recipes? (ex: RSS as recipe)
