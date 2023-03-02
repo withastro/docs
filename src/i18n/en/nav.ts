69bce680ffcfd4733577c5c3190f4fa223e8798f
Nate Moore <nate@astro.build>
2023-03-02 16:34:09 -0600

    feat: add assets page

--- src/i18n/en/nav.ts
+++ src/i18n/en/nav.ts
@@ -69,6 +69,7 @@ export default [
 		key: 'guides/content-collections',
 	},
 	{ text: 'Connecting a CMS', slug: 'guides/cms', key: 'guides/cms' },
+	{ text: 'Assets (Experimental)', slug: 'guides/assets', key: 'guides/assets' },
 	{ text: 'Images', slug: 'guides/images', key: 'guides/images' },
 	{ text: 'Fonts', slug: 'guides/fonts', key: 'guides/fonts' },
 	{
