34f268a1ed6125a08cfb02839d172ac7d7cad443
Elib27 <92606530+Elib27@users.noreply.github.com>
2023-05-23 22:45:05 +0200

    i18n(fr): Translation of `backend.mdx` (#3234)
    * Create backend.mdx
    
    * Update backend.mdx
    
    * Update backend.mdx
    
    * Update backend.mdx
    
    * Update nav.ts

--- src/i18n/fr/nav.ts
+++ src/i18n/fr/nav.ts
@@ -31,6 +31,7 @@ export default NavDictionary({
 	examples: 'Recettes',
 	'guides/cms': 'Utiliser un CMS avec Astro',
 	'guides/migrate-to-astro': 'Migrer vers Astro',
+  'guides/backend': 'Ajouter des services Backend',
 	'guides/integrations-guide': 'Intégrations',
 	'guides/deploy': 'Déployer',
 
