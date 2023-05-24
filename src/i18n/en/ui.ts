fa7f848416d58f1dc72029d4ffe0540034678773
Chris Swithinbank <swithinbank@gmail.com>
2023-05-24 21:25:40 +0200

    Remove outdated translation note (#3329)
    * Run git once
    
    * Try dumb version of `getEntryBySlug`
    
    * Revert "Try dumb version of `getEntryBySlug`"
    
    This reverts commit 37630ab2cea245a1b4f26ff7a0141a80afa532b3.
    
    * Cache git results in translation status builder
    
    * Remove outdated notice
    
    * Discard changes to scripts/lib/translation-status/builder.ts
    
    ---------

--- src/i18n/en/ui.ts
+++ src/i18n/en/ui.ts
@@ -63,9 +63,6 @@ export default {
 	'fallbackContent.notice':
 		'This page is not yet available in your language, so we’re showing you the English version. You can help by translating it!',
 	'fallbackContent.linkText': 'Learn more about how you can contribute',
-	// Outdated content notice shown when a page is outdated
-	'outdatedContent.notice':
-		'The content on this page is an older translation that requires updating to match the current English version. You can help by translating it!',
 	// 404 Page
 	'404.title': 'Not Found',
 	'404.content': 'This page isn’t in our solar system.',
