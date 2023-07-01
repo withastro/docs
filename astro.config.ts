0960b3642794ee1bd28b9e9effae8bb4daafaba3
hippotastic <6137925+hippotastic@users.noreply.github.com>
2023-07-01 16:13:13 +0200

    Migrate code blocks to Expressive Code

--- astro.config.ts
+++ astro.config.ts
@@ -8,31 +8,28 @@ import rehypeSlug from 'rehype-slug';
 import remarkSmartypants from 'remark-smartypants';
 
 import { asideAutoImport, astroAsides } from './integrations/astro-asides';
-import { astroCodeSnippets, codeSnippetAutoImport } from './integrations/astro-code-snippets';
+import { astroDocsExpressiveCode } from './integrations/expressive-code';
 import { sitemap } from './integrations/sitemap';
 import { autolinkConfig } from './plugins/rehype-autolink-config';
 import { rehypei18nAutolinkHeadings } from './plugins/rehype-i18n-autolink-headings';
 import { rehypeOptimizeStatic } from './plugins/rehype-optimize-static';
 import { rehypeTasklistEnhancer } from './plugins/rehype-tasklist-enhancer';
 import { remarkFallbackLang } from './plugins/remark-fallback-lang';
-import { theme } from './syntax-highlighting-theme';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://docs.astro.build/',
 	integrations: [
 		AutoImport({
-			imports: [asideAutoImport, codeSnippetAutoImport],
+			imports: [asideAutoImport],
 		}),
 		preact({ compat: true }),
 		sitemap(),
 		astroAsides(),
-		astroCodeSnippets(),
+		astroDocsExpressiveCode(),
 		mdx(),
 	],
 	markdown: {
-		syntaxHighlight: 'shiki',
-		shikiConfig: { theme },
 		// Override with our own config
 		smartypants: false,
 		remarkPlugins: [
