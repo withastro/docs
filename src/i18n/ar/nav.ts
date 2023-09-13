9ee8c8f4eb774ac2aa7042c043967a9b91b2f2ba
omarr45 <58887202+omarr45@users.noreply.github.com>
2023-09-13 12:38:43 +0300

    i18n(ar): update `nav.ts` according to glossary

--- src/i18n/ar/nav.ts
+++ src/i18n/ar/nav.ts
@@ -1,15 +1,11 @@
-/*
- * This configures the navigation sidebar.
- * Only add entries for pages that are translated into Arabic.
- */
-
 import { NavDictionary } from '../translation-checkers';
 
 export default NavDictionary({
 	// Start Here
 	startHere: 'ابدأ هنا',
 	'getting-started': 'دليل البداية',
-	'editor-setup': 'إعداد البيئة البرمجية',
+	install: 'التثبيت',
+	'editor-setup': 'إعداد المحرر',
 	'guides/upgrade-to/v3': 'الترقية إلى الإصدار 3',
 
 	// Core Concepts
@@ -26,53 +22,53 @@ export default NavDictionary({
 	'core-concepts/project-structure': 'هيكل المشروع',
 	'core-concepts/astro-components': 'المكوّنات',
 	'core-concepts/astro-pages': 'الصفحات',
-	'core-concepts/layouts': 'التخطيطات',
+	'core-concepts/layouts': 'النسق',
 
 	// Examples
 	examples: 'الأمثلة',
 	'guides/migrate-to-astro': 'التحويل إلى أسترو',
 	'guides/cms': 'ربط نظام إدارة المحتوى',
-	'guides/backend': 'إضافة خدمات الخلف',
+	'guides/backend': 'إضافة خدمات قفويّة',
 	'guides/integrations-guide': 'إضافة التكاملات',
 	'guides/deploy': 'نشر موقعك',
 	'guides/recipes': 'المزيد من الأمثلة',
 
 	// Guides
 	features: 'الدليل',
-	'core-concepts/astro-syntax': 'صيغة كتابة أسترو',
+	'core-concepts/astro-syntax': 'التركيب النحوي لأسترو',
 	'core-concepts/framework-components': 'مكوّنات أطر الويب الأخرى',
 	'core-concepts/routing': 'التوجيه',
-	'guides/markdown-content': 'المحتوى بصيغة ماركداون و MDX',
+	'guides/markdown-content': 'ماركداون و MDX',
 	'guides/content-collections': 'مجموعات المحتوى',
-	'guides/client-side-scripts': 'النصوص ومعالجة الأحداث',
-	'guides/styling': 'التصميم و CSS',
+	'guides/client-side-scripts': 'الإخطاطات وتناول الأحداث',
+	'guides/styling': 'الطُرُز و CSS',
 	'guides/images': 'الصور',
 	'guides/fonts': 'الخطوط',
 	'guides/imports': 'الإضافات',
 	'guides/server-side-rendering': 'التصيير على الخادم',
-	'core-concepts/endpoints': 'نقاط النهاية',
+	'core-concepts/endpoints': 'النقاط الطرفية',
 	'guides/data-fetching': 'جلب البيانات',
 	'guides/middleware': 'الوسيط',
-	'guides/testing': 'الفحص',
-	'guides/view-transitions': 'تحول الصفحات',
-	'guides/troubleshooting': 'إصلاح المشاكل',
+	'guides/testing': 'الاختبار',
+	'guides/view-transitions': 'انتقال المشهد',
+	'guides/troubleshooting': 'حل المشاكل',
 
 	// Configuration
 	configuration: 'الإعدادات',
 	'guides/configuring-astro': 'ملف إعدادات أسترو',
-	'guides/typescript': 'تايب سكربت',
+	'guides/typescript': 'TypeScript',
 	'guides/aliases': 'الأسماء المستعارة للإضافات',
 	'guides/environment-variables': 'متغيرات البيئة',
 
 	// Reference
 	reference: 'المراجع',
 	'reference/configuration-reference': 'الإعدادات',
-	'reference/api-reference': 'واجهة برمجة التطبيقات وقت التشغيل',
-	'reference/integrations-reference': 'واجهة برمجة التطبيقات للتكاملات',
-	'reference/adapter-reference': 'واجهة برمجة التطبيقات للمحولات',
-	'reference/image-service-reference': 'واجهة برمجة التطبيقات لخدمة الصور',
+	'reference/api-reference': 'API زمن التنفيذ',
+	'reference/integrations-reference': 'API التكاملات',
+	'reference/adapter-reference': 'API الموائِم',
+	'reference/image-service-reference': 'API خدمة الصور',
 	'reference/directives-reference': 'توجيهات القوالب',
-	'reference/cli-reference': 'واجهة سطر الأوامر لأسترو',
+	'reference/cli-reference': 'واجهة سطر الأوامر (CLI) لأسترو',
 	'reference/error-reference': 'مرجع الأخطاء',
 	'guides/publish-to-npm': 'تنسيق حزمة NPM',
 });
