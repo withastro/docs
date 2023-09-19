85c7cc56515b115afa721dc2562907f57073ad72
NightFeather <leolee50910@gmail.com>
2023-09-20 02:05:26 +0800

    Update docsearch.ts

--- src/i18n/zh-tw/docsearch.ts
+++ src/i18n/zh-tw/docsearch.ts
@@ -2,7 +2,7 @@ import { DocSearchDictionary } from '../translation-checkers';
 
 export default DocSearchDictionary({
 	button: '搜尋',
-	placeholder: '搜尋文件',
+	placeholder: '搜尋文檔',
 	shortcutLabel: '按下 / 來搜尋',
 	resultsFooterLede: '正在尋找 Astro 整合或主題？需要更多幫助嗎？',
 	resultsFooterIntegrations: 'Astro 整合目錄',
