74e7285b807d16f2b4b0f63249a061b082b69e9a
JS Park <junseongpark.dev@gmail.com>
2023-10-15 08:49:21 +0900

    i18n(ko): update `ui.ts`

--- src/i18n/ko/ui.ts
+++ src/i18n/ko/ui.ts
@@ -1,96 +1,128 @@
 import { UIDictionary } from '../translation-checkers';
 
 export default UIDictionary({
-	'a11y.skipLink': '본문 바로가기',
-	'a11y.sectionLink': '섹션 제목',
-	'navbar.a11yTitle': '맨 위로',
-	// Site settings >>>> REVIEW: 리뷰 필요
+	'a11y.skipLink': '콘텐츠로 이동',
+	'a11y.sectionLink': '섹션 제목: ',
+	'navbar.a11yTitle': '탐색 메뉴',
+	// Site settings
 	'site.title': 'Astro 문서',
-	'site.description': '클라이언트 사이드 자바스크립트를 줄이고, 더 빠른 웹 사이트를 구축하기.',
+	'site.description': '더 적은 클라이언트 측 JavaScript로 더 빠른 웹사이트를 구축하세요.',
 	'site.og.imageSrc': '/default-og-image.png?v=1',
 	'site.og.imageAlt':
-		'별이 쏟아지는 광활한 우주에 Astro 로고가 있고 오른쪽 전경에 보라색 토성 모양의 행성이 떠 있습니다.',
+		'오른쪽 전경에 보라색 토성과 같은 행성이 떠 있는 별이 빛나는 넓은 우주에 있는 Astro 로고',
 	// Left Sidebar
-	'leftSidebar.a11yTitle': '주 메뉴',
+	'leftSidebar.a11yTitle': '왼쪽 사이드바',
 	'leftSidebar.learnTab': '배우기',
-	'leftSidebar.referenceTab': '참고자료',
+	'leftSidebar.referenceTab': '참조',
 	'leftSidebar.viewInEnglish': '영어로 보기',
-	'leftSidebar.sponsoredBy': '스폰서',
+	'leftSidebar.sponsoredBy': '후원',
 	// Right Sidebar
-	'rightSidebar.a11yTitle': '보조 메뉴',
+	'rightSidebar.a11yTitle': '오른쪽 사이드바',
 	'rightSidebar.onThisPage': '목차',
 	'rightSidebar.overview': '개요',
 	'rightSidebar.community': '커뮤니티',
-	'rightSidebar.joinDiscord': 'Discord 참여하기',
-	'rightSidebar.readBlog': 'Astro 공식 블로그',
-	'rightSidebar.openCollective': 'Open Collective',
+	'rightSidebar.joinDiscord': 'Discord',
+	'rightSidebar.readBlog': '블로그',
+	'rightSidebar.openCollective': 'Our Open Collective',
 	'rightSidebar.contribute': '기여하기',
-	'rightSidebar.editPage': '이 페이지 편집하기',
-	'rightSidebar.translatePage': '이 페이지 번역하기',
-	'rightSidebar.github': 'Astro 문서의 GitHub',
+	'rightSidebar.contributorGuides': '기여자를 위한 안내서',
+	'rightSidebar.editPage': '페이지 수정',
+	'rightSidebar.translatePage': '이 페이지 번역',
+	'rightSidebar.github': 'GitHub에서 Astro 문서 보기',
 	// Footer
-	'footer.privacyPolicy': '개인정보보호정책',
+	'footer.privacyPolicy': '개인 정보 정책',
 	// `<ThemeToggleButton>` acessibility labels
-	'themeToggle.useLight': '밝은 테마 사용',
-	'themeToggle.useDark': '어두운 테마 사용',
+	'themeToggle.useLight': '밝은 테마',
+	'themeToggle.useDark': '어두운 테마',
 	// Used in previous/next page links at the bottom of pages
 	'articleNav.nextPage': '다음 페이지',
 	'articleNav.prevPage': '뒤로',
 	// Used in `<Since>`: Added in: v0.24.0 [NEW]
-	'since.addedIn': '추가됨:',
-	'since.new': '신규',
-	'since.beta': '베타',
+	'since.addedIn': 'Added in:',
+	'since.new': 'New',
+	'since.beta': 'Beta',
 	// Installation Guide
-	'install.autoTab': 'Automatic CLI',
-	'install.manualTab': 'Manual Setup',
+	'install.autoTab': '자동 CLI',
+	'install.manualTab': '수동 설치',
 	// `<DeployGuidesNav>` vocabulary
-	'deploy.sectionTitle': '배포 가이드',
-	'deploy.altSectionTitle': '배포 가이드 더 보기',
-	'deploy.filterLabel': '배포 유형별 필터',
+	'deploy.sectionTitle': '배포 안내서',
+	'deploy.altSectionTitle': '더 많은 배포 안내서',
+	'deploy.filterLabel': '배포 타입에 따라 분류',
 	'deploy.ssrTag': 'SSR',
 	'deploy.staticTag': 'Static',
 	// CMS Guides vocabulary
-	'cms.navTitle': 'CMS 가이드 더 보기',
+	'cms.navTitle': '더 많은 CMS 안내서',
 	// Migration Guides vocabulary
-	'migration.navTitle': '마이그레이션 가이드 더 보기',
+	'migration.navTitle': '더 많은 전환 안내서',
+	// Recipes vocabulary
+	'recipes.navTitle': '더 많은 레시피',
+	// `<RecipeLinks>` vocabulary
+	'recipesLink.singular': '관련 레시피:',
+	'recipesLink.plural': '관련 레시피',
 	// `<ContributorList>` fallback text
-	'contributors.seeAll': '모든 기여자 보기',
+	'contributors.seeAll': '모든 기여자',
 	// Fallback content notice shown when a page is not yet translated
 	'fallbackContent.notice':
-		'이 페이지는 아직 한국어로 제공되지 않으므로 영어 버전을 보여드립니다. 번역 활동에 참여해보세요!',
-	'fallbackContent.linkText': '기여 방법에 대해 자세히 알아보기',
+		'이 페이지는 아직 한글로 번역되지 않았으므로 영어 버전의 페이지를 보게 됩니다. 번역에 참여해주세요.',
+	'fallbackContent.linkText': '번역에 참여하는 방법',
 	// 404 Page
-	'404.title': '페이지를 찾을 수 없습니다',
+	'404.title': '찾을 수 없음',
 	'404.content': '이 페이지는 우리 태양계에 존재하지 않습니다.',
-	'404.linkText': '기존 차원으로 돌아가기',
+	'404.linkText': '홈페이지로 이동',
 	// Aside component default labels
-	'aside.note': '노트',
+	'aside.note': '참고',
 	'aside.tip': '팁',
-	'aside.caution': '주의',
-	'aside.danger': '위험',
+	'aside.caution': '경고',
+	'aside.danger': '주의',
+	// `<LanguageSelect>` vocabulary
+	'languageSelect.label': '언어 선택',
 	// Integrations vocabulary
 	'integrations.changelog': '변경 로그',
 	'integrations.footerTitle': '더 많은 통합',
 	'integrations.renderers': 'UI 프레임워크',
 	'integrations.adapters': 'SSR 어댑터',
 	'integrations.others': '기타',
-	// Checklist component >>>> TODO: 컨텍스트 확인 못함
+	// Checklist component
 	'checklist.or': '또는',
-	// Multiple Choice component >>>> TODO: 컨텍스트 확인 못함
-	'multipleChoice.defaultCorrect': '정답!',
-	'multipleChoice.defaultIncorrect': '다시 시도해보세요!',
-	'multipleChoice.submitLabel': '제출하기',
-	// Tutorial Progress >>>> TODO: 컨텍스트 확인 못함
-	'progress.todo': 'To do',
+	// Multiple Choice component
+	'multipleChoice.defaultCorrect': '정확해요!',
+	'multipleChoice.defaultIncorrect': '다시 선택하세요!',
+	'multipleChoice.submitLabel': '제출',
+	// Tutorial Progress
+	'progress.todo': '수행할 작업',
 	'progress.done': '완료',
-	// Tutorial Navigation >>>> TODO: 컨텍스트 확인 못함
-	'tutorial.trackerLabel': '튜토리얼 트래커',
+	// Tutorial Navigation
+	'tutorial.trackerLabel': '튜토리얼 진행도',
 	'tutorial.unit': '단위',
-	// Tutorial >>>> TODO: 컨텍스트 확인 못함
-	'tutorial.getReady': '준비…',
-	// Feedback Fish widget >>>> TODO: 컨텍스트 확인 못함
-	'feedback.button': '의견을 보내주세요',
+	// Tutorial
+	'tutorial.getReady': 'Get ready to…',
+	// Feedback Fish widget
+	'feedback.button': 'Give us feedback',
+	'feedback.a11yLabel': 'Feedback form',
+	'feedback.formTitle': 'What’s on your mind?',
+	'feedback.categoryGroupLabel': 'Choose feedback category',
+	'feedback.issue': 'Issue',
+	'feedback.createIssue': 'Create GitHub Issue',
+	'feedback.idea': 'Idea',
+	'feedback.other': 'Other',
+	'feedback.messageA11yLabel': 'Message',
+	'feedback.placeholder': 'What do you want us to know?',
+	'feedback.submit': 'Send feedback',
+	'feedback.close': 'Close feedback form',
+	'feedback.success': 'Thanks! We received your feedback.',
 	// `<FileTree>` component
-	'fileTree.directoryLabel': '디렉토리',
-	// BaaS Guides vocabulary
+	'fileTree.directoryLabel': 'Directory',
+	// Code snippet vocabulary
+	'expressiveCode.terminalWindowFallbackTitle': 'Terminal window',
+	'expressiveCode.copyButtonTooltip': 'Copy to clipboard',
+	'expressiveCode.copyButtonCopied': 'Copied!',
+	// Backend Guides vocabulary
+	'backend.navTitle': 'More backend service guides',
+	// Stubs vocabulary
+	'stub.title': 'Expand this stub!',
+	'stub.subtitle': 'This guide is a stub.',
+	'stub.description.migration':
+		'Want to contribute to this guide? Have a blog post, video, or another resource to share about migrating from this technology to Astro?',
+	'stub.description.cms': 'Know more about how to use this CMS with Astro?',
+	'stub.description.backend': 'Know more about how to use this backend service with Astro?',
 });
