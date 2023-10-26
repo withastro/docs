d663699bea4082a903930b02be953d44a0bcf080
ElianCodes <ElianCodes@users.noreply.github.com>
2023-10-26 12:34:29 +0000

    [ci] format

--- src/i18n/hi/nav.ts
+++ src/i18n/hi/nav.ts
@@ -1,11 +1,11 @@
 import { NavDictionary } from '../translation-checkers';
 
 export default NavDictionary({
-	  // Start Here
-		startHere: 'यहाँ से शुरू करे',
-		'getting-started': 'पहले कदम',
-	
-		// Core Concepts
-		coreConcepts: 'मूल अवधारणाएं',
-		'concepts/why-astro': 'क्यों Astro',
+	// Start Here
+	startHere: 'यहाँ से शुरू करे',
+	'getting-started': 'पहले कदम',
+
+	// Core Concepts
+	coreConcepts: 'मूल अवधारणाएं',
+	'concepts/why-astro': 'क्यों Astro',
 });
