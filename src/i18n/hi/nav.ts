3ac91523201924b224937306b7de1707684c8aaa
Rohit Kushwaha <technicalrohit06@gmail.com>
2023-11-10 15:39:36 +0530

    update nav.ts for hindi translated content

--- src/i18n/hi/nav.ts
+++ src/i18n/hi/nav.ts
@@ -1,11 +1,74 @@
 import { NavDictionary } from '../translation-checkers';
 
 export default NavDictionary({
-	// Start Here
 	startHere: 'यहाँ से शुरू करे',
 	'getting-started': 'पहले कदम',
+	install: 'स्थापित करें',
+	'editor-setup': 'संपादक सेटअप',
+	'guides/upgrade-to/v3': 'v3 में अपग्रेड करें',
 
-	// Core Concepts
-	coreConcepts: 'मूल अवधारणाएं',
-	'concepts/why-astro': 'क्यों Astro',
+	coreConcepts: 'मूल अवधारणाएँ',
+	'concepts/why-astro': 'Astro क्यों',
+	'concepts/islands': 'Astro द्वीप',
+
+	tutorials: 'शिक्षण सत्र',
+	'blog-tutorial': 'ब्लॉग बनाएं',
+	'add-collections-tutorial': 'सामग्री संग्रह बढ़ाएं',
+	'add-transitions-tutorial': 'दृश्य परिसर बढ़ाएं',
+
+	basics: 'मौलिक अवधारणाएँ',
+	'core-concepts/project-structure': 'परियोजना संरचना',
+	'core-concepts/astro-components': 'घटक',
+	'core-concepts/astro-pages': 'पृष्ठ',
+	'core-concepts/layouts': 'नक़्शे',
+	'core-concepts/astro-syntax': 'Astro की सिंटैक्स',
+
+	builtins: 'सामग्री स्थापित',
+	'guides/content-collections': 'सामग्री संग्रह',
+	'guides/view-transitions': 'view transitions',
+
+	addons: 'एड-ऑन्स',
+	'guides/integrations-guide': 'एकीकरण जानकारी',
+	'core-concepts/framework-components': 'UI फ़्रेमवर्क्स',
+	'guides/server-side-rendering': 'सर्वर-साइड रेंडरिंग (SSR)',
+
+	examples: 'उदाहरण',
+	'guides/migrate-to-astro': 'Astro में माइग्रेट करें',
+	'guides/cms': 'एक CMS से कनेक्ट करें',
+	'guides/backend': 'बैकएंड सेवाओं को जोड़ें',
+	'guides/deploy': 'अपनी साइट को डिप्लॉय करें',
+	'guides/recipes': 'अधिक रेसिपी',
+
+	features: 'गाइड्स',
+	'core-concepts/routing': 'रूटिंग',
+	'guides/markdown-content': 'मार्कडाउन और MDX',
+	'guides/client-side-scripts': 'स्क्रिप्ट और घटना संचालन',
+	'guides/styling': 'स्टाइल और CSS',
+	'guides/images': 'छवियाँ',
+	'guides/fonts': 'फ़ॉन्ट्स',
+	'guides/imports': 'आयात',
+	'core-concepts/endpoints': 'एंडपॉइंट्स',
+	'guides/data-fetching': 'डेटा फ़ेचिंग',
+	'guides/internationalization': 'अंतर्राष्ट्रीयकरण',
+	'guides/middleware': 'मिडलवेयर',
+	'guides/testing': 'टेस्टिंग',
+	'guides/troubleshooting': 'समस्या समाधान',
+
+	configuration: 'कॉन्फ़िगरेशन',
+	'guides/configuring-astro': 'Astro कॉन्फ़िगरेशन फ़ाइल',
+	'guides/typescript': 'टाइपस्क्रिप्ट',
+	'guides/aliases': 'आयात उपनाम',
+	'guides/environment-variables': 'पर्यावरण चर',
+
+	reference: 'संदर्भ',
+	'reference/configuration-reference': 'कॉन्फ़िगरेशन',
+	'reference/api-reference': 'रनटाइम API',
+	'reference/integrations-reference': 'एकीकरण API',
+	'reference/adapter-reference': 'एडाप्टर API',
+	'reference/image-service-reference': 'इमेज सेवा API',
+	'reference/dev-overlay-plugin-reference': 'डेवलपर ओवरले प्लगइन API',
+	'reference/directives-reference': 'टेम्पलेट निर्देशिकाएँ',
+	'reference/cli-reference': 'Astro CLI',
+	'reference/error-reference': 'त्रुटि संदर्भ',
+	'guides/publish-to-npm': 'NPM पैकेज प्रारूप',
 });
