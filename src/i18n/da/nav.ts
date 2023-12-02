2201ebfcea169666c69902a8b3bfbeec3f6f8629
kevinanielsen <keveran@gmail.com>
2023-12-02 08:36:40 +0100

    add danish translation for nav

---
+++ src/i18n/da/nav.ts
@@ -0,0 +1,73 @@
+import { NavDictionary } from '../translation-checkers';
+
+export default NavDictionary({
+	// Start Here
+	startHere: 'Begynd her',
+	'getting-started': 'Kom i gang',
+	install: 'Installation',
+	'editor-setup': 'Editor-opsætning',
+	'guides/upgrade-to/v3': 'Opgrader til Astro v3',
+
+	// Core Concepts
+	coreConcepts: 'Kernekoncepter',
+	'concepts/islands': 'Astro-øer',
+	'concepts/why-astro': 'Hvorfor Astro',
+
+	// Tutorial
+	tutorials: 'Tutorials',
+	'blog-tutorial': 'Byg en blog',
+
+	// Basics
+	basics: 'Grundlæggende',
+	'core-concepts/project-structure': 'Projektstruktur',
+	'core-concepts/astro-components': 'Komponenter',
+	'core-concepts/astro-pages': 'Sider',
+	'core-concepts/layouts': 'Layouts',
+
+	// Recipes
+	examples: 'Instruktioner',
+	'guides/migrate-to-astro': 'Migrær til Astro',
+	'guides/cms': 'Forbind med CMS',
+	'guides/backend': 'Tilføj backend-tjenester',
+	'guides/integrations-guide': 'Guide til integrationer',
+	'guides/deploy': 'Offentligør websiden',
+	'guides/recipes': 'Flere instruktioner',
+
+	// Features
+	features: 'Funktioner',
+	'core-concepts/astro-syntax': 'Astro-Syntax',
+	'core-concepts/framework-components': 'UI-Frameworks',
+	'core-concepts/routing': 'Routing',
+	'guides/markdown-content': 'Markdown & MDX',
+	'guides/content-collections': 'Content-samlinger',
+	'guides/client-side-scripts': 'Scripts og eventhåndtering',
+	'guides/styling': 'CSS & Styling',
+	'guides/images': 'Billeder',
+	'guides/fonts': 'Skriftstyper',
+	'guides/imports': 'Importeringer',
+	'guides/server-side-rendering': 'Serverside-rendering (SSR)',
+	'core-concepts/endpoints': 'Endepunkter',
+	'guides/data-fetching': 'Data indlæsning',
+	'guides/middleware': 'Middleware',
+	'guides/testing': 'Testning',
+	'guides/view-transitions': 'View Transitions',
+	'guides/troubleshooting': 'Problemsøgning',
+	// 'guides/rss': 'RSS',
+
+	configuration: 'Konfiguration',
+	'guides/configuring-astro': 'Astro konfigurationsfilen',
+	'guides/typescript': 'TypeScript',
+	'guides/aliases': 'Import-Aliasnavne',
+	'guides/environment-variables': 'Miljøvariabler',
+
+	// Reference
+	reference: 'Refference',
+	'reference/configuration-reference': 'Konfiguration',
+	'reference/api-reference': 'Runtime-API',
+	'reference/integrations-reference': 'Integrations-API',
+	'reference/adapter-reference': 'Adapter-API',
+	'reference/image-service-reference': 'Billedetjeneste-API',
+	'reference/directives-reference': 'Skabelonsdirektiver',
+	'reference/cli-reference': 'Kommandolinjegrænseflade (CLI)',
+	'reference/error-reference': 'Fejlrefference',
+	'guides/publish-to-npm': 'NPM-Pakkeformat',});
