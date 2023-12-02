afa601a76852a33a97c0177d9696a0793fbcb875
kevinanielsen <keveran@gmail.com>
2023-12-02 08:23:45 +0100

    add danish translation for docsearch

---
+++ src/i18n/da/docsearch.ts
@@ -0,0 +1,12 @@
+import { DocSearchDictionary } from '../translation-checkers';
+
+export default DocSearchDictionary({
+	button: 'Søg',
+	placeholder: 'Søg documentationen',
+	shortcutLabel: 'Tryk / for at søge',
+	resultsFooterLede: 'Leder du efter en Astro integration eller tema? Brug for mere hjælp?',
+	resultsFooterIntegrations: 'Astro integrations vejviser',
+	resultsFooterThemes: 'Astro tema fremvisning',
+	resultsFooterDiscord: 'Kom med på Discord',
+	modal: {},
+});
