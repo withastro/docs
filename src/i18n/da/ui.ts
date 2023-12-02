7cacc49ed6dba262cbea186f1536c7b95ce0b14c
kevinanielsen <keveran@gmail.com>
2023-12-02 08:54:30 +0100

    add danish translation for ui

---
+++ src/i18n/da/ui.ts
@@ -0,0 +1,128 @@
+import { UIDictionary } from '../translation-checkers';
+
+export default UIDictionary({
+	'a11y.skipLink': 'Spring videre til indhold',
+	'a11y.sectionLink': 'Afsnit med titlen',
+	'navbar.a11yTitle': 'Top',
+	// Site settings
+	'site.title': 'Astro Dokumentation',
+	'site.description': 'Byg hurtigere hjemmesider med mindre klientside JavaScript.',
+	'site.og.imageSrc': '/default-og-image.png?v=1',
+	'site.og.imageAlt':
+		'astro logo i et stjernefyldt rum med en lilla saturn-agtig planet, der svæver i forgrunden',
+	// Left Sidebar
+	'leftSidebar.a11yTitle': 'Primær',
+	'leftSidebar.learnTab': 'Lær',
+	'leftSidebar.referenceTab': 'Refference',
+	'leftSidebar.viewInEnglish': 'Vis på engelsk',
+	'leftSidebar.sponsoredBy': 'Sponsoreret af',
+	// Right Sidebar
+	'rightSidebar.a11yTitle': 'Sekundær',
+	'rightSidebar.onThisPage': 'På denne side',
+	'rightSidebar.overview': 'Oversigt',
+	'rightSidebar.community': 'Fælleskab',
+	'rightSidebar.joinDiscord': 'Kom med os på Discord',
+	'rightSidebar.readBlog': 'Læs vores blogindslag',
+	'rightSidebar.openCollective': 'Åben vores samling',
+	'rightSidebar.contribute': 'Hjælp til',
+	'rightSidebar.contributorGuides': 'Guides til at hjælpe',
+	'rightSidebar.editPage': 'Rediger denne side',
+	'rightSidebar.translatePage': 'Oversæt denne side',
+	'rightSidebar.github': 'Astro Docs på GitHub',
+	// Footer
+	'footer.privacyPolicy': 'Fortrolighedspolitik',
+	// `<ThemeToggleButton>` acessibility labels
+	'themeToggle.useLight': 'Brug lyst tema',
+	'themeToggle.useDark': 'Brug mørkt tema',
+	// Used in previous/next page links at the bottom of pages
+	'articleNav.nextPage': 'Næste side',
+	'articleNav.prevPage': 'Tilbage',
+	// Used in `<Since>`: Added in: v0.24.0 [NEW]
+	'since.addedIn': 'Tilføjet i:',
+	'since.new': 'Ny',
+	'since.beta': 'Beta',
+	// Installation Guide
+	'install.autoTab': 'Automatisk CLI',
+	'install.manualTab': 'Manuelt Setup',
+	// `<DeployGuidesNav>` vocabulary
+	'deploy.sectionTitle': 'Offentliggørelses Guides',
+	'deploy.altSectionTitle': 'Flere offentliggøreslses Guides',
+	'deploy.filterLabel': 'Filtrer med offentliggørelses-type',
+	'deploy.ssrTag': 'SSR',
+	'deploy.staticTag': 'Statisk',
+	// CMS Guides vocabulary
+	'cms.navTitle': 'Flere CMS guides',
+	// Migration Guides vocabulary
+	'migration.navTitle': 'Flere migrations guides',
+	// Recipes vocabulary
+	'recipes.navTitle': 'More instruktioner',
+	// `<RecipeLinks>` vocabulary
+	'recipesLink.singular': 'Relaterede instruktioner:',
+	'recipesLink.plural': 'Relaterede instruktioner',
+	// `<ContributorList>` fallback text
+	'contributors.seeAll': 'Se alle medvirkende',
+	// Fallback content notice shown when a page is not yet translated
+	'fallbackContent.notice':
+		'Denne side er endnu ikke tilgængeligt på dit sprog, så vi viser dig den engelske version. Du kan hjælpe os med at oversætte den!',
+	'fallbackContent.linkText': 'Lær mere om hvordan du kan hjælpe til',
+	// 404 Page
+	'404.title': 'Ikke fundet',
+	'404.content': 'Denne side er ikke i vores solsystem.',
+	'404.linkText': 'Tag mig hjem.',
+	// Aside component default labels
+	'aside.note': 'Note',
+	'aside.tip': 'Tip',
+	'aside.caution': 'Pas på',
+	'aside.danger': 'Fare',
+	// `<LanguageSelect>` vocabulary
+	'languageSelect.label': 'Vælg sprog',
+	// Integrations vocabulary
+	'integrations.changelog': 'Changelog',
+	'integrations.footerTitle': 'Flere integrationer',
+	'integrations.renderers': 'UI Frameworks',
+	'integrations.adapters': 'SSR Adaptere',
+	'integrations.others': 'Andre',
+	// Checklist component
+	'checklist.or': 'eller',
+	// Multiple Choice component
+	'multipleChoice.defaultCorrect': 'Korrekt!',
+	'multipleChoice.defaultIncorrect': 'Prøv igen!',
+	'multipleChoice.submitLabel': 'Indsend',
+	// Tutorial Progress
+	'progress.todo': 'To-do',
+	'progress.done': 'Færdig',
+	// Tutorial Navigation
+	'tutorial.trackerLabel': 'Tutorial Tracker',
+	'tutorial.unit': 'Enhed',
+	// Tutorial
+	'tutorial.getReady': 'Vær klar på…',
+	// Feedback Fish widget
+	'feedback.button': 'Giv os feedback',
+	'feedback.a11yLabel': 'Feedback formular',
+	'feedback.formTitle': 'Hvad har du på hjerte?',
+	'feedback.categoryGroupLabel': 'Vælg feedback kategori',
+	'feedback.issue': 'Problem',
+	'feedback.createIssue': 'Lav et GitHub Issue',
+	'feedback.idea': 'Idé',
+	'feedback.other': 'Andre',
+	'feedback.messageA11yLabel': 'Beskeder',
+	'feedback.placeholder': 'Hvad skal vi vide?',
+	'feedback.submit': 'Send feedback',
+	'feedback.close': 'Luk feedback formular',
+	'feedback.success': 'Tak! Vi har modtaget din feedback.',
+	// `<FileTree>` component
+	'fileTree.directoryLabel': 'Vejviser',
+	// Code snippet vocabulary
+	'expressiveCode.terminalWindowFallbackTitle': 'Terminal vindue',
+	'expressiveCode.copyButtonTooltip': 'Kopier til udkipsholder',
+	'expressiveCode.copyButtonCopied': 'Kopieret!',
+	// Backend Guides vocabulary
+	'backend.navTitle': 'Flere backend-service guider',
+	// Stubs vocabulary
+	'stub.title': 'Udvid denne stub!',
+	'stub.subtitle': 'Denne guide er en stub.',
+	'stub.description.migration':
+		'Vil du hjælpe os med denne guide? Har du et blog indlæg, video eller andre resourcer at dele om migration fra denne teknologi til Astro?',
+	'stub.description.cms': 'Ved du mere om hvordan man bruger denne CMS med Astro?',
+	'stub.description.backend': 'Ved du mere om hvordan man bruger denne backend-service med Astro?',
+});
