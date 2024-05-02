cc191b7aecc87d8de9d88624de324b8048d861c7
Matthew Phillips <matthew@skypack.dev>
2024-05-02 10:40:23 -0400

    Test enabling content collections cache

--- astro.config.ts
+++ astro.config.ts
@@ -92,7 +92,7 @@ export default defineConfig({
 		service: sharpImageService(),
 	},
 	experimental: {
-		contentCollectionCache: false,
+		contentCollectionCache: true,
 		directRenderScript: true,
 	},
 });
