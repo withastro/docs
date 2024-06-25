07c0dee5bbc6f0bb2a28dd649fdccd338491f300
yanthomasdev <yanthomasdev@users.noreply.github.com>
2024-06-25 19:08:52 +0000

    [ci] format

--- src/i18n/es/nav.ts
+++ src/i18n/es/nav.ts
@@ -1,18 +1,18 @@
 import { NavDictionary } from '../translation-checkers';
 
 export default NavDictionary({
-	'welcomeWorld': '¡Hola Mundo!',
+	welcomeWorld: '¡Hola Mundo!',
 	'getting-started': 'Cómo Empezar',
 	'install-and-setup': 'Instalación y Configuración',
 	'guides/deploy': 'Despliega tu Aplicación',
 	'guides/migrate-to-astro': 'Migra a Astro',
 	'blog-tutorial': 'Tutorial: Construye un Blog',
 
-	'coreConcepts': 'Conceptos Principales ',
+	coreConcepts: 'Conceptos Principales ',
 	'concepts/why-astro': '¿Por qué Astro?',
 	'concepts/islands': 'Islas de Astro',
 
-	'basics': 'Aprende lo Básico',
+	basics: 'Aprende lo Básico',
 	'basics/project-structure': 'Estructura del proyecto',
 	'basics/astro-components': 'Componentes',
 	'basics/astro-pages': 'Páginas',
@@ -22,26 +22,26 @@ export default NavDictionary({
 	'guides/imports': 'Importaciones',
 	'guides/integrations-guide': 'Integraciones',
 
-	'content': 'Administra tu Contenido',
+	content: 'Administra tu Contenido',
 	'guides/content': 'Creación de Contenido',
 	'guides/markdown-content': 'Markdown',
 	'guides/content-collections': 'Colecciones de Contenido',
 	'guides/cms': 'Conectar un CMS',
 	'guides/rss': 'Agregar un feed RSS',
 	'guides/view-transitions': 'Transiciones de vista',
-	'routes': 'Rutas y Navegación',
+	routes: 'Rutas y Navegación',
 	'guides/routing': 'Enrutamiento',
 	'guides/endpoints': 'Endpoints',
 	'guides/prefetch': 'Precarga',
 	'guides/middleware': 'Middleware',
 	'guides/internationalization': 'Internacionalización',
 
-	'assets': 'Recursos',
+	assets: 'Recursos',
 	'guides/styling': 'Estilos y CSS',
 	'guides/images': 'Imágenes',
 	'guides/fonts': 'Fuentes',
 
-	'data': 'Conecta Tus Datos',
+	data: 'Conecta Tus Datos',
 	'guides/data-fetching': 'Obtención de Datos',
 	'guides/astro-db': 'Astro DB',
 	'guides/backend': 'Añade Servicios de Backend',
@@ -49,16 +49,16 @@ export default NavDictionary({
 	'guides/authentication': 'Autenticación',
 	'guides/environment-variables': 'Variables de Entorno',
 
-	'clientSide': 'Interactividad del lado del cliente',
+	clientSide: 'Interactividad del lado del cliente',
 	'guides/framework-components': 'UI Frameworks',
 	'guides/client-side-scripts': 'Scripts y Manejadores de Eventos',
 
-	'maintain': 'Mantener Tu Proyecto',
+	maintain: 'Mantener Tu Proyecto',
 	'upgrade-astro': 'Actualiza Astro',
 	'guides/testing': 'Pruebas',
 	'guides/troubleshooting': 'Resolución de problemas',
 
-	'reference': 'Referencia',
+	reference: 'Referencia',
 	'reference/configuration-reference': 'Configuración',
 	'reference/api-reference': 'API de Tiempo de Ejecución de Astro',
 	'reference/cli-reference': 'CLI de Astro',
@@ -66,20 +66,14 @@ export default NavDictionary({
 	'guides/typescript': 'Referencia de TypeScript',
 	'reference/error-reference': 'Referencia de Errores',
 
-	'dev': 'Otras APIs de Desarrollo',
+	dev: 'Otras APIs de Desarrollo',
 	'reference/integrations-reference': 'API de Integraciones',
 	'reference/adapter-reference': 'API de Adaptadores',
 	'reference/image-service-reference': 'API de Servicio de Imagen',
 	'reference/dev-toolbar-app-reference': 'API de Herramientas de desarrollo',
 	'reference/container-reference': 'API de Contenedor (Experimental)',
 
-	'communityResources': 'Recursos de la Comunidad',
+	communityResources: 'Recursos de la Comunidad',
 	'community-resources/content': 'Cursos, Guías y Recetas',
-	'community-resources/talks': 'Charlas, Entrevistas y Transmisiones'
-
-
-
-
-
-
+	'community-resources/talks': 'Charlas, Entrevistas y Transmisiones',
 });
