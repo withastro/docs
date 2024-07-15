6f8226719daac1da288dd97683bd69016ddcd2d3
Chris Swithinbank <swithinbank@gmail.com>
2024-07-15 16:29:49 +0200

    Add integration to handle caching images

---
+++ integrations/netlify-cache.ts
@@ -0,0 +1,22 @@
+import { save, restore } from '@netlify/cache-utils';
+import type { AstroIntegration } from 'astro';
+
+const cacheDirectories = ['./node_modules/.astro', './node_modules/.astro-og-canvas'];
+
+export const netlifyCache = (): AstroIntegration => ({
+	name: 'astro-netlify-cache',
+	hooks: {
+		async 'astro:build:start'({ logger }) {
+			if (!process.env.NETLIFY) return;
+			logger.info(`Restoring: ${cacheDirectories.join(', ')}`);
+			await restore(cacheDirectories);
+			logger.info(`Cache restored.`);
+		},
+		async 'astro:build:done'({ logger }) {
+			if (!process.env.NETLIFY) return;
+			logger.info(`Caching: ${cacheDirectories.join(', ')}`);
+			await save(cacheDirectories);
+			logger.info(`Directories cached.`);
+		},
+	},
+});
