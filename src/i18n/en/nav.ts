94cc65a36650fb4dbb2beb21e5725d2fe87f5c2c
Sarah Rainsberger <sarah@rainsberger.ca>
2024-09-07 19:57:25 +0000

    remove placeholder mdx content file

--- src/i18n/en/nav.ts
+++ src/i18n/en/nav.ts
@@ -44,7 +44,6 @@ export default [
 	{ text: 'Content collections', slug: 'guides/content-collections', key: 'guides/content-collections' },
 	{ text: 'Connect a CMS', slug: 'guides/cms', key: 'guides/cms' },
 	{ text: 'Add an RSS feed', slug: 'guides/rss', key: 'guides/rss' },
-	// { text: 'MDX', slug: 'guides/mdx-content', key: 'guides/mdx-content' },
 
 	{ text: 'Routes and Navigation', header: true, type: 'learn', key: 'routes' },
 	{ text: 'Routing', slug: 'guides/routing', key: 'guides/routing' },
