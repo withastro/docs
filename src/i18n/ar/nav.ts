4b14ba670b2540fb02c6ac8a6985a9959cc36d48
yanthomasdev <yanthomasdev@users.noreply.github.com>
2024-10-10 18:03:01 +0000

    [ci] format

--- src/i18n/ar/nav.ts
+++ src/i18n/ar/nav.ts
@@ -7,7 +7,7 @@ export default NavDictionary({
 	'guides/deploy': 'نشر موقعك',
 	'guides/migrate-to-astro': 'التحويل إلى أسترو',
 	'blog-tutorial': 'إنشاء مدونة',
-	
+
 	coreConcepts: 'المفاهيم الأساسية',
 	'concepts/why-astro': 'لماذا أسترو؟',
 	'concepts/islands': 'جزر أسترو',
@@ -28,7 +28,7 @@ export default NavDictionary({
 	'guides/content-collections': 'مجموعات المحتوى',
 	'guides/cms': 'ربط نظام إدارة المحتوى',
 	'guides/rss': 'إضافة تغذية RSS',
-	
+
 	routes: 'المسارات و التنقل',
 	'guides/routing': 'التوجيه',
 	'guides/endpoints': 'النقاط الطرفية',
@@ -37,13 +37,13 @@ export default NavDictionary({
 	'guides/middleware': 'الوسيط',
 	'guides/internationalization': 'تدويل',
 	'guides/view-transitions': 'إنتقالات المشهد',
-	
+
 	assets: 'الموارد',
 	'guides/styling': 'الأنماط و CSS',
 	'guides/images': 'الصور',
 	'guides/fonts': 'الخطوط',
 	'guides/media': 'ربط الوسائط المستضافة أو DAM',
-	
+
 	data: 'اربط بياناتك',
 	'guides/data-fetching': 'جلب البيانات',
 	'guides/astro-db': 'قاعدة بيانات أسترو',
@@ -51,16 +51,16 @@ export default NavDictionary({
 	'guides/ecommerce': 'التجارة الإلكترونية',
 	'guides/authentication': 'المصادقة',
 	'guides/environment-variables': 'متغيرات البيئة',
-	
+
 	clientSide: 'تفاعل جهة العميل',
 	'guides/framework-components': 'إطار واجهة المستخدم',
 	'guides/client-side-scripts': 'النصوص البرمجية و معالجة الأحداث',
-	
+
 	maintain: 'صيانة مشروعك',
 	'upgrade-astro': 'ترقية أسترو',
 	'guides/testing': 'الاختبار',
 	'guides/troubleshooting': 'إكتشاف و إصلاح الأخطاء',
-	
+
 	reference: 'المراجع',
 	'reference/configuration-reference': 'الإعدادات',
 	'reference/api-reference': 'API زمن التنفيذ',
@@ -68,18 +68,17 @@ export default NavDictionary({
 	'reference/directives-reference': 'مرجع التعليمات',
 	'guides/typescript': 'مرجع TypeScript',
 	'reference/error-reference': 'مرجع الأخطاء',
-	
+
 	dev: 'API التطوير الأخرى',
 	'reference/integrations-reference': 'API التكاملات',
 	'reference/adapter-reference': 'API الموائِم',
 	'reference/image-service-reference': 'API خدمة الصور',
 	'reference/dev-toolbar-app-reference': 'API شريط أدوات التطوير',
 	'reference/container-reference': 'API الحاويات (تجريبية)',
-	
+
 	communityResources: 'موارد المجتمع',
 	'community-resources/content': 'الدروس والأدلة والوصفات',
 	'community-resources/talks': 'المحادثات, المقابلات و البثوث',
-
 
 	// ## legacy ## :
 	// startHere: 'ابدأ هنا',
