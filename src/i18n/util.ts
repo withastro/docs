433199716c9362f5455d4305af09b3222bd939ef
Chris Swithinbank <swithinbank@gmail.com>
2024-11-04 23:44:08 +0100

    Use Starlight UI strings in search component

--- src/i18n/util.ts
+++ src/i18n/util.ts
@@ -1,6 +1,4 @@
-import type { AstroGlobal } from 'astro';
-import { getLanguageFromURL } from '../util';
-import type { DocSearchTranslation, NavDict } from './translation-checkers';
+import type { NavDict } from './translation-checkers';
 
 /**
  * Convert the map of modules returned by `import.meta.globEager` to an object
@@ -15,18 +13,8 @@ function mapDefaultExports<T>(modules: Record<string, { default: T }>) {
 	return exportMap;
 }
 
-const docsearchTranslations = mapDefaultExports<DocSearchTranslation>(
-	import.meta.glob('./*/docsearch.ts', { eager: true })
-);
 export const navTranslations = mapDefaultExports<NavDict>(
 	import.meta.glob('./*/nav.ts', { eager: true })
 );
 
 export const fallbackLang = 'en';
-
-/** Returns a dictionary of strings for use with DocSearch. */
-export function getDocSearchStrings(Astro: AstroGlobal): DocSearchTranslation {
-	const lang = getLanguageFromURL(Astro.url.pathname) || fallbackLang;
-	// A shallow merge is sufficient here as most of the actual fallbacks are provided by DocSearch.
-	return { ...docsearchTranslations[fallbackLang], ...docsearchTranslations[lang] };
-}
