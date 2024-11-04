9d42bf6aeb5973df277989641aafa92810e52693
Chris Swithinbank <swithinbank@gmail.com>
2024-11-05 00:14:20 +0100

    Remove DocSearchDictionary utility

--- src/i18n/translation-checkers.ts
+++ src/i18n/translation-checkers.ts
@@ -1,8 +1,5 @@
-import type docsearch from '@docsearch/js';
 import enNav from './en/nav';
 import type { allLanguages } from './languages';
-
-type ModalTranslations = NonNullable<Parameters<typeof docsearch>[0]['translations']>['modal'];
 
 export type UILanguageKeys = keyof typeof allLanguages;
 
@@ -31,22 +28,3 @@ export const NavDictionary = (dict: Partial<Record<NavDictionaryKeys, string>>) 
 	}
 	return orderedDictionary;
 };
-
-export interface DocSearchTranslation {
-	// These two keys are Astro-specific and apply to the search box in the header.
-	button?: string;
-	shortcutLabel?: string;
-	// Astro-specific labels for the custom `resultsFooterComponent`.
-	resultsFooterLede?: string;
-	resultsFooterIntegrations?: string;
-	resultsFooterThemes?: string;
-	resultsFooterDiscord?: string;
-	// Search box placeholder text within the DocSearch modal.
-	placeholder?: string;
-	// This object follows DocSearch's translation.modal format.
-	// See: https://docsearch.algolia.com/docs/api/#translations
-	modal?: ModalTranslations;
-}
-
-/** Helper to type check a dictionary of DocSearch string translations. */
-export const DocSearchDictionary = (dict: DocSearchTranslation) => dict;
