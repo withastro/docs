01f9515815f8ed0c0c18442edc1a546c48637b5d
Chris Swithinbank <swithinbank@gmail.com>
2024-11-14 13:31:56 +0100

    Add beginning of English nav labels dictionary

---
+++ src/content/nav/en.ts
@@ -0,0 +1,26 @@
+export default {
+	start: 'Start',
+	'start.welcome': 'Welcome, World!',
+	'start.newProject': 'Start a new project',
+	'start.config': 'Configuration',
+	'start.migrate': 'Migrate to Astro',
+
+	guides: 'Guides and Recipes',
+	'guides.routing': 'Routing and Navigation',
+	'guides.ui': 'Build your UI',
+	'guides.content': 'Add content to your site',
+	'guides.serverRendering': 'Server rendering',
+	'guides.upgrade': 'Upgrade',
+	'guides.upgrade.major': 'Major upgrade guides',
+	'guides.recipes': 'How-to recipes',
+
+	reference: 'Reference',
+	'reference.configuration': 'Configuration',
+	'reference.runtime': 'Runtime API',
+	'reference.cli': 'CLI',
+	'reference.errors': 'Errors',
+	'reference.errors.messages': 'Error messages',
+	'reference.other': 'Other development APIs',
+
+	integrations: 'integrations',
+};
