5d9b923aa0facc002773ecc0bbbc5ae5e8d0013b
Chris Swithinbank <swithinbank@gmail.com>
2024-11-15 12:21:12 +0100

    Watch `./config/*` directory for file changes in dev

--- astro.config.ts
+++ astro.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
 	site,
 	integrations: [
 		devServerFileWatcher([
+			'./config/*', // Custom plugins and integrations
 			'./astro.sidebar.ts', // Sidebar configuration file
 			'./src/content/nav/*.ts', // Sidebar labels
 		]),
