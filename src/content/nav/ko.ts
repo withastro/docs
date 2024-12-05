1c3def347e4dbf4c7ee20dfcdea9a1dff463b05e
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2024-12-05 11:19:53 +0900

    update

--- src/content/nav/ko.ts
+++ src/content/nav/ko.ts
@@ -7,14 +7,14 @@ export default navDictionary({
 	'start.config': '구성',
 	'start.migrate': 'Astro로 마이그레이션',
 
-	guides: '가이드 및 활용 예제',
-	'guides.routing': '라우팅 및 내비게이션',
+	guides: '가이드 및 레시피',
+	'guides.routing': '라우팅 및 탐색',
 	'guides.ui': 'UI 구축',
 	'guides.content': '사이트에 콘텐츠 추가',
 	'guides.serverRendering': '서버 렌더링',
 	'guides.upgrade': '업그레이드',
 	'guides.upgrade.major': '주요 업그레이드 가이드',
-	'guides.recipes': '실습 가이드',
+	'guides.recipes': '활용 레시피',
 
 	reference: '참조',
 	'reference.runtime': '런타임 API',
@@ -27,7 +27,7 @@ export default navDictionary({
 	'integrations.adapters': '어댑터',
 	'integrations.other': '기타 공식 통합',
 
-	thirdParty: '서드파티 서비스',
+	thirdParty: '타사 서비스',
 	'thirdParty.deployment': '배포 가이드',
 	'thirdParty.cms': '콘텐츠 관리 시스템',
 	'thirdParty.backend': '백엔드 서비스',
