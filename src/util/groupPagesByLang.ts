0f94b68634327a87a537ea5be475c951392ff87b
Chris Swithinbank <swithinbank@gmail.com>
2024-12-20 16:16:46 +0100

    Update content collection use: `id` instead of `slug`, imported `render()` method

--- src/util/groupPagesByLang.ts
+++ src/util/groupPagesByLang.ts
@@ -3,7 +3,7 @@ import type { CollectionEntry } from 'astro:content';
 export const groupPagesByLang = <T extends CollectionEntry<'docs'>>(pages: T[]) =>
 	pages.reduce(
 		(pages, page) => {
-			const lang = page.slug.split('/')[0];
+			const lang = page.id.split('/')[0];
 			if (!pages[lang]) pages[lang] = [];
 			pages[lang].push(page);
 			return pages;
