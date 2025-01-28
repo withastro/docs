e60f9c1182d58c515377c77efd0400473955c135
Emanuele Stoppa <my.burning@gmail.com>
2025-01-28 14:51:03 +0000

    update sidebar

--- astro.sidebar.ts
+++ astro.sidebar.ts
@@ -135,6 +135,7 @@ export const sidebar = [
 					'reference/experimental-flags/client-prerender',
 					'reference/experimental-flags/content-intellisense',
 					'reference/experimental-flags/sessions',
+					'reference/experimental-flags/serialized-configuration',
 				],
 			}),
 			'reference/legacy-flags',
