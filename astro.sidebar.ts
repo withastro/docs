5382e8876029f97ed15d0d08ac76ef28563fc9a7
sarah11918 <5098874+sarah11918@users.noreply.github.com>
2025-06-24 17:39:32 +0000

    [ci] format

--- astro.sidebar.ts
+++ astro.sidebar.ts
@@ -15,7 +15,12 @@ export const sidebar = [
 		items: [
 			'getting-started',
 			group('start.welcome', {
-				items: ['concepts/why-astro', 'concepts/islands', 'tutorial/0-introduction', 'astro-courses'],
+				items: [
+					'concepts/why-astro',
+					'concepts/islands',
+					'tutorial/0-introduction',
+					'astro-courses',
+				],
 			}),
 			group('start.newProject', {
 				items: ['install-and-setup', 'basics/project-structure', 'develop-and-build'],
