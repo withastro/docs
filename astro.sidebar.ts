97738b5339cd3ebf7f915c006c4b96f205568100
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-10-03 11:20:17 +0200

    feat: stabilize static import meta env

--- astro.sidebar.ts
+++ astro.sidebar.ts
@@ -149,7 +149,6 @@ export const sidebar = [
 					'reference/experimental-flags/content-intellisense',
 					'reference/experimental-flags/preserve-scripts-order',
 					'reference/experimental-flags/heading-id-compat',
-					'reference/experimental-flags/static-import-meta-env',
 					'reference/experimental-flags/chrome-devtools-workspace',
 				],
 			}),
