d3a58f5f39552414b758855afa980676ed174b88
nemu <58832428+erbierc@users.noreply.github.com>
2025-11-10 16:10:15 +0100

    i18n(pl): add navigation label translation

--- src/content/nav/pl.ts
+++ src/content/nav/pl.ts
@@ -1,5 +1,35 @@
 import { navDictionary } from '../../util/navDictionary';
 
 export default navDictionary({
-	// Add translations of English labels here
+	start: 'Start',
+	'start.welcome': 'Witaj, świecie!',
+	'start.newProject': 'Rozpocznij nowy projekt',
+	'start.config': 'Konfiguracja',
+	'start.migrate': 'Migracja do Astro',
+
+	guides: 'Przewodniki i przykłady',
+	'guides.routing': 'Routing i nawigacja',
+	'guides.ui': 'Zbuduj swój interfejs użytkownika',
+	'guides.content': 'Dodaj treści do swojej strony',
+	'guides.serverRendering': 'Renderowanie po stronie serwera',
+	'guides.upgrade': 'Aktualizacje',
+	'guides.upgrade.major': 'Przewodniki po ważnych aktualizacjach',
+	'guides.recipes': 'Przykłady zastosowań',
+
+	reference: 'Odniesienia',
+	'reference.runtime': 'API Runtime',
+	'reference.other': 'Inne API dla programistów',
+	'reference.syntax': 'Składnia szablonów Astro',
+	'reference.experimental': 'Funkcje eksperymentalne',
+
+	integrations: 'Integracje',
+	'integrations.ui': 'Frameworki UI',
+	'integrations.adapters': 'Adaptery',
+	'integrations.other': 'Inne oficjalne integracje',
+
+	thirdParty: 'Usługi stron trzecich',
+	'thirdParty.deployment': 'Przewodniki wdrażania',
+	'thirdParty.cms': 'Systemy zarządzania treściami',
+	'thirdParty.backend': 'Usługi backendowe',
+	'thirdParty.media': 'Hostowanie obrazów i wideo',
 });
