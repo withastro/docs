74e2d8528110400e86b93785d68d08ed6b7c0080
oldiberezkoo <agerycraftservermine@gmail.com>
2026-02-17 14:22:22 +0500

    [agents.md]

--- config/locales.ts
+++ config/locales.ts
@@ -6,23 +6,12 @@ import type { StarlightUserConfig } from '@astrojs/starlight/types';
  */
 export const localesConfig = {
 	en: { label: 'English', lang: 'en', dir: 'ltr' },
-	de: { label: 'Deutsch', lang: 'de', dir: 'ltr' },
-	'pt-br': { label: 'Português do Brasil', lang: 'pt-BR', dir: 'ltr' },
-	es: { label: 'Español', lang: 'es', dir: 'ltr' },
-	'zh-cn': { label: '简体中文', lang: 'zh-CN', dir: 'ltr' },
-	'zh-tw': { label: '正體中文', lang: 'zh-TW', dir: 'ltr' },
-	fr: { label: 'Français', lang: 'fr', dir: 'ltr' },
-	hi: { label: 'हिन्दी', lang: 'hi', dir: 'ltr' },
-	ar: { label: 'العربية', lang: 'ar', dir: 'rtl' },
-	ja: { label: '日本語', lang: 'ja', dir: 'ltr' },
-	ko: { label: '한국어', lang: 'ko', dir: 'ltr' },
-	pl: { label: 'Polski', lang: 'pl', dir: 'ltr' },
 	ru: { label: 'Русский', lang: 'ru', dir: 'ltr' },
-	it: { label: 'Italiano', lang: 'it', dir: 'ltr' },
+
 } satisfies StarlightUserConfig['locales'];
 
 /** The only two languages to build to speed up Astro's smoke tests. */
-const twoLanguages: (keyof typeof localesConfig)[] = ['en', 'ko'];
+const twoLanguages: (keyof typeof localesConfig)[] = ['en', 'ru'];
 
 /** Starlight i18n configuration used for Astro's smoke tests. */
 export const twoLocalesConfig = Object.fromEntries(
